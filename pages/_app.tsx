1e2626b0034223a49a9999582ebb351194966f4a
tamagoyakii <rlawlgus4063@naver.com>
2023-04-05 14:51:37 +0900

    [feat] 프로젝트 초기 세팅 close #1

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
+import 'styles/globals.css';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
