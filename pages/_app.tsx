2bc0b177de63c1a0be2319d3d07d2d4850260211
tamagoyakii <rlawlgus4063@naver.com>
2023-04-06 21:40:54 +0900

    [feat] recoil 추가 #1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+
+import type { AppProps } from 'next/app';
+
+import Layout from 'components/layouts/Layout';
+
 import 'styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </RecoilRoot>
   );
 }
