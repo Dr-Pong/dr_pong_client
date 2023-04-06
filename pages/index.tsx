d2bb7afbf9dc7f29826ef206f0cbadddde1c70c2
triplecheeseburger <hangyulkim94@gmail.com>
2023-04-07 02:16:30 +0900

    [feat] page.json 업데이트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import '../i18n';
+import 'i18n';
+import MyPageFrame from "../components/myPage/MyPageFrame";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <MyPageFrame />
     </div>
   );
 }
