ddac77efbc418824f6e6d0e035977818c3b2412f
tamagoyakii <rlawlgus4063@naver.com>
2023-04-12 18:56:37 +0900

    [feat] 모달 상태를 관리하기 위한 recoil 추가 #4

---
+++ recoils/modal.ts
@@ -0,0 +1,14 @@
+import { atom } from 'recoil';
+import { v1 } from 'uuid';
+
+import { ModalParts } from 'types/modalTypes';
+
+export const modalPartsState = atom<ModalParts>({
+  key: `modalPartsState/${v1()}`,
+  default: { head: null, body: null, tail: null },
+});
+
+export const openModalState = atom<boolean>({
+  key: `openModalState/${v1()}`,
+  default: false,
+});
