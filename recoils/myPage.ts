ae6c16d50df0481acbb1345b38aa3fe0b0577f17
triplecheeseburger <hangyulkim94@gmail.com>
2023-04-18 21:16:07 +0900

    [feat] userState Recoil 추가, 어딘가에 적용해야 함

--- recoils/myPage.ts
+++ recoils/myPage.ts
@@ -1,7 +1,17 @@
 import { atom } from 'recoil';
 import { v1 } from 'uuid';
 
+import { User } from '../types/myPageTypes';
+
 export const editableState = atom<boolean>({
   key: `editableState/${v1()}`,
   default: false,
+});
+
+export const userState = atom<User>({
+  key: `userState/${v1()}`,
+  default: {
+    nickname: '',
+    imgUrl: '',
+  },
 });
