84612298ee7078fa27aa0a6d955a38cec04368a2
tamagoyakii <rlawlgus4063@naver.com>
2023-04-19 17:26:36 +0900

    [refactor] 버튼 배열로 수정 및 그에 따른 스타일 처리 close #33

--- components/layouts/NavigationBar.tsx
+++ components/layouts/NavigationBar.tsx
@@ -5,29 +5,32 @@ import { IoMdChatboxes, IoMdHome, IoMdPerson } from 'react-icons/io';
 
 import styles from 'styles/layouts/NavigationBar.module.scss';
 
+type navigation = {
+  content: string | React.ReactNode;
+  route: string;
+  style: 'basic' | 'big';
+};
+
 export default function NavigationBar() {
+  const navigations: navigation[] = [
+    { content: <IoMdHome />, route: '/', style: 'basic' },
+    { content: <IoMdChatboxes />, route: '/channels', style: 'basic' },
+    { content: 'Game\nStart', route: '/game', style: 'big' },
+    { content: <FaHandsHelping />, route: '/friends', style: 'basic' },
+    { content: <IoMdPerson />, route: '/myPage', style: 'basic' },
+  ];
   return (
     <div className={styles.navigationBarContainer}>
-      <Link href='/' className={styles.navigationButton}>
-        <IoMdHome />
-      </Link>
-      <Link href='/channels' className={styles.navigationButton}>
-        <IoMdChatboxes />
-      </Link>
-      <Link
-        href='/game'
-        className={`${styles.navigationButton} ${styles.mainButton}`}
-      >
-        Game
-        <br />
-        Start
-      </Link>
-      <Link href='/friends' className={styles.navigationButton}>
-        <FaHandsHelping />
-      </Link>
-      <Link href='/myPage' className={styles.navigationButton}>
-        <IoMdPerson />
-      </Link>
+      {navigations.map(({ content, route, style }) => {
+        return (
+          <Link
+            href={route}
+            className={`${styles.navigationButton} ${styles[style]}`}
+          >
+            {content}
+          </Link>
+        );
+      })}
     </div>
   );
 }
