03636856d2b51f99522bcc09cf8c3af1b452fb95
tamagoyakii <rlawlgus4063@naver.com>
2023-04-20 18:05:04 +0900

    [feat] button 구현 close #20

---
+++ components/global/buttons/BasicButton.tsx
@@ -0,0 +1,34 @@
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
+
+import { modalPartsState, openModalState } from 'recoils/modal';
+
+import styles from 'styles/global/Button.module.scss';
+
+type CancelButtonProps = {
+  style: string;
+  color: string;
+  value: string;
+  handleButtonClick: React.FormEventHandler<HTMLFormElement> | null;
+};
+
+export default function BasicButton({
+  style,
+  color,
+  value,
+  handleButtonClick,
+}: CancelButtonProps) {
+  const setOpenModal = useSetRecoilState(openModalState);
+  const resetModalParts = useResetRecoilState(modalPartsState);
+  const handleModalClose = () => {
+    setOpenModal(false);
+    resetModalParts();
+  };
+
+  return (
+    <form onClick={handleButtonClick || handleModalClose}>
+      <button className={`${styles[style]} ${styles[color]}`} type='button'>
+        {value}
+      </button>
+    </form>
+  );
+}
---
+++ components/global/buttons/SubmitButton.tsx
@@ -0,0 +1,23 @@
+import styles from 'styles/global/Button.module.scss';
+
+type SubmitButtonProps = {
+  style: string;
+  color: string;
+  value: string;
+  handleButtonClick: React.FormEventHandler<HTMLFormElement>;
+};
+
+export default function SubmitButton({
+  style,
+  color,
+  value,
+  handleButtonClick,
+}: SubmitButtonProps) {
+  return (
+    <form onSubmit={handleButtonClick}>
+      <button className={`${styles[style]} ${styles[color]}`} type='submit'>
+        {value}
+      </button>
+    </form>
+  );
+}
