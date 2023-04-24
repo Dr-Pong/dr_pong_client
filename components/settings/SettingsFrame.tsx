0ccc28a83191d9e6cdd34ed091a5a32be2f38848
tamagoyakii <rlawlgus4063@naver.com>
2023-04-24 20:19:18 +0900

    [feat] SettingsFrame 컴포넌트 분리 #42

---
+++ components/settings/SettingsFrame.tsx
@@ -0,0 +1,70 @@
+import useTranslation from 'next-translate/useTranslation';
+
+import { useRouter } from 'next/router';
+
+import RadioButtons from 'components/global/RadioButtons';
+import BasicButton from 'components/global/buttons/BasicButton';
+
+import styles from 'styles/settings/settings.module.scss';
+
+export default function SettingsFrame() {
+  const { t } = useTranslation('settings');
+  const router = useRouter();
+  const { locale, defaultLocale, pathname } = router;
+  const currentLocale = locale || defaultLocale || 'en';
+
+  const localeOptions = [
+    { id: 'en', value: 'English' },
+    { id: 'ko', value: '한국어' },
+  ];
+  const authOptions = [
+    { id: 'enable', value: t('enable') },
+    { id: 'disable', value: t('disable') },
+  ];
+
+  const changeLocale = (lang: string | undefined) => {
+    router.push(pathname, pathname, { locale: lang || defaultLocale });
+  };
+
+  const changeSecondAuth = () => {
+    // patch
+  };
+
+  const saveSettings = () => {
+    changeLocale(
+      document.querySelector('input[type=radio][name=locale]:checked')?.id
+    );
+    changeSecondAuth();
+  };
+
+  return (
+    <div className={styles.settingListContainer}>
+      <ul>
+        <li>
+          <div className={styles.listTopic}>{t('Language')}</div>
+          <RadioButtons
+            name='locale'
+            options={localeOptions}
+            currentId={currentLocale}
+          />
+        </li>
+        <li>
+          <div className={styles.listTopic}>{t('2nd Authentication')}</div>
+          <RadioButtons
+            name='secondAuth'
+            options={authOptions}
+            currentId='enable'
+          />
+        </li>
+      </ul>
+      <div className={styles.saveButton}>
+        <BasicButton
+          style='basic'
+          color='black'
+          value='저장'
+          handleButtonClick={saveSettings}
+        />
+      </div>
+    </div>
+  );
+}
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,55 +1,17 @@
-import $ from 'jquery';
 import useTranslation from 'next-translate/useTranslation';
 
-import { useRouter } from 'next/router';
-
 import PageHeader from 'components/global/PageHeader';
-import BasicButton from 'components/global/buttons/BasicButton';
+import SettingsFrame from 'components/settings/SettingsFrame';
 
-import styles from 'styles/pages/settings.module.scss';
+import styles from 'styles/settings/settings.module.scss';
 
 export default function settings() {
   const { t } = useTranslation('settings');
-  const locales = [
-    { locale: 'en', value: 'English' },
-    { locale: 'ko', value: '한국어' },
-  ];
-  const router = useRouter();
-  const { locale, defaultLocale, pathname } = router;
-  const currentLocale = locale || defaultLocale;
-
-  const changeLocale = (lang: string | undefined) => {
-    router.push(pathname, pathname, { locale: lang || defaultLocale });
-  };
-
-  const saveSettings = () => {
-    changeLocale($('input:radio[name=locale]:checked').val()?.toString());
-  };
 
   return (
     <div className={styles.settingsPageContainer}>
       <PageHeader title={t('Settings')} button={null} />
-      <div>{t('Language')}</div>
-      {locales.map(({ locale, value }) => {
-        return (
-          <span>
-            <input
-              type='radio'
-              name='locale'
-              id={locale}
-              value={locale}
-              defaultChecked={currentLocale === locale}
-            />
-            <label htmlFor={locale}>{value}</label>
-          </span>
-        );
-      })}
-      <BasicButton
-        style='basic'
-        color='black'
-        value='저장'
-        handleButtonClick={saveSettings}
-      />
+      <SettingsFrame />
     </div>
   );
 }
