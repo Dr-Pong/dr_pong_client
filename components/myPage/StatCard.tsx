14c0882b5ff49878c55bd3a1771829d0e1d388d6
triplecheeseburger <hangyulkim94@gmail.com>
2023-04-25 16:11:02 +0900

    [feat] StatCard에 History 페이지로 가는 링크 만들기 close #58

--- components/myPage/StatCard.tsx
+++ components/myPage/StatCard.tsx
@@ -1,5 +1,7 @@
 import useTranslation from 'next-translate/useTranslation';
 
+import Link from 'next/link';
+
 import React from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
 
@@ -46,8 +48,10 @@ export default function StatCard({ userName }: { userName: string }) {
           <WinRateStat winRateInfo={totalStat} />
         </div>
         <div className={styles.box} id={styles.history}>
-          <div className={styles.historyButton}>{t('History')}</div>
-          <IoIosArrowForward />
+          <Link href={`matchHistory?${userName}`}>
+            <div className={styles.historyButton}>{t('History')}</div>
+            <IoIosArrowForward />
+          </Link>
         </div>
       </div>
       <div className={styles.container} id={styles.rank}>
