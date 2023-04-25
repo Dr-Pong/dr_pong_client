b4d20d15dbc2589191325a0b0f9d6fcae8bad3ef
tamagoyakii <rlawlgus4063@naver.com>
2023-04-25 20:36:12 +0900

    [feat] basicButton, closeModalButton 분리 #55

--- components/global/buttons/BasicButton.tsx
+++ components/global/buttons/BasicButton.tsx
@@ -1,7 +1,3 @@
-import { useResetRecoilState, useSetRecoilState } from 'recoil';
-
-import { modalPartsState, openModalState } from 'recoils/modal';
-
 import { ButtonProps } from 'types/buttonTypes';
 
 import styles from 'styles/global/Button.module.scss';
@@ -12,15 +8,8 @@ export default function BasicButton({
   handleButtonClick,
   children,
 }: ButtonProps) {
-  const setOpenModal = useSetRecoilState(openModalState);
-  const resetModalParts = useResetRecoilState(modalPartsState);
-  const handleModalClose = () => {
-    setOpenModal(false);
-    resetModalParts();
-  };
-
   return (
-    <form onClick={handleButtonClick || handleModalClose}>
+    <form onClick={handleButtonClick ?? (() => {})}>
       <button className={`${styles[style]} ${styles[color]}`} type='button'>
         {children}
       </button>
