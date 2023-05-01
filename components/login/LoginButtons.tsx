45cb5903dcdc1f67532970d74a16a7d19c8a1190
tamagoyakii <rlawlgus4063@naver.com>
2023-05-01 17:40:39 +0900

    [refactor] 컴포넌트 이름 변경 및 스타일 파일 적용 #55

--- components/login/LoginButtons.tsx
+++ components/login/LoginButtons.tsx
@@ -4,9 +4,9 @@ import { FcGoogle } from 'react-icons/fc';
 
 import BasicButton from 'components/global/buttons/BasicButton';
 
-import styles from 'styles/login/Login.module.scss';
+import styles from 'styles/login/LoginButtons.module.scss';
 
-export default function LoginFrame() {
+export default function LoginButtons() {
   const { t } = useTranslation('login');
   const oauths = [
     {
@@ -18,7 +18,7 @@ export default function LoginFrame() {
         />
       ),
       value: t('Login with 42Intra'),
-      link: 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-9178ebee1b15304ba011ca8b7e3b17306c8c6b8c5e6d2bf5bfda8c6c57b5a24b&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2F42%2Flogin&response_type=code',
+      link: 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-9178ebee1b15304ba011ca8b7e3b17306c8c6b8c5e6d2bf5bfda8c6c57b5a24b&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin%2F42&response_type=code',
     },
     {
       name: 'Google',
@@ -28,13 +28,13 @@ export default function LoginFrame() {
     },
   ];
   return (
-    <div className={styles.loginFrame}>
+    <div className={styles.loginButtons}>
       {oauths.map(({ name, logo, value, link }) => {
         return (
           <BasicButton key={name} style='big' color='white'>
             <a href={link}>
-              <span>{logo}</span>
-              <span>{value}</span>
+              <span className={styles.logo}>{logo}</span>
+              <span className={styles.string}>{value}</span>
             </a>
           </BasicButton>
         );
