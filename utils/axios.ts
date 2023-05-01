81cf007eb8f070d730c33e146d3fa2b69fbb068e
tamagoyakii <rlawlgus4063@naver.com>
2023-05-01 15:54:52 +0900

    [feat] 헤더의 Authorization에 토큰 추가 #55

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,7 +1,25 @@
 import axios from 'axios';
 
+import { useCookies } from 'react-cookie';
+
 const baseURL = `http://localhost:3000/api`;
 
 const instance = axios.create({ baseURL });
+
+const [cookies] = useCookies(['Authorization']);
+
+instance.interceptors.request.use(
+  function setConfig(parameter) {
+    const config = parameter;
+    const token = cookies.Authorization;
+
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+  },
+  function getError(error) {
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
