5298a69ead3b91f8b5a0b33d2122edece0665797
tamagoyakii <rlawlgus4063@naver.com>
2023-05-01 19:14:06 +0900

    [fix] 에러나서 주석처리 #55

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
-import { useCookies } from 'react-cookie';
+// import { useCookies } from 'react-cookie';
 
 const baseURL = `http://localhost:3000/api`;
 
 const instance = axios.create({ baseURL });
 
-const [cookies] = useCookies(['Authorization']);
+// const [cookies] = useCookies(['Authorization']) || [];
 
-instance.interceptors.request.use(
-  function setConfig(parameter) {
-    const config = parameter;
-    const token = cookies.Authorization;
+// instance.interceptors.request.use(
+//   function setConfig(parameter) {
+//     const config = parameter;
+//     const token = cookies?.Authorization;
 
-    config.headers['Content-Type'] = 'application/json';
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+//     config.headers['Content-Type'] = 'application/json';
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
 
-    return config;
-  },
-  function getError(error) {
-    return Promise.reject(error);
-  }
-);
+//     return config;
+//   },
+//   function getError(error) {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default instance;
