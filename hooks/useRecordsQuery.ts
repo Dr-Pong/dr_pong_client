242331712220e44a52931ca2e6e3048b14812c4b
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-02 21:31:58 +0900

    [feat] Records용 쿼리 훅 생성

---
+++ hooks/useRecordsQuery.ts
@@ -0,0 +1,22 @@
+import { Record, Records } from 'types/historyTypes';
+
+import useCustomQuery from 'hooks/useCustomQuery';
+
+import instance from '../utils/axios';
+
+const useRecordsQuery = (userName: string) => {
+  const { get } = useCustomQuery();
+  const fetchMatchHistory = async (lastGameId: number): Promise<Records> => {
+    const count = 10;
+    return (
+      await instance.get(
+        `/users/${userName}/records?lastGameId=${lastGameId}&count=${count}`
+      )
+    ).data;
+  };
+  const getMatchDetail = (gameId: number) => {
+    return get([`matchDetail`, gameId], `/users/${userName}/records/${gameId}`);
+  };
+  return { fetchMatchHistory, getMatchDetail };
+};
+export default useRecordsQuery;
