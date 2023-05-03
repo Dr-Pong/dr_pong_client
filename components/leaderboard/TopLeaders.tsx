17e9892b83728d4fe684edacbcbbc83c2b83268a
tamagoyakii <rlawlgus4063@naver.com>
2023-05-03 19:59:23 +0900

    [feat] 프로필 모달 추가(임시) #95

--- components/leaderboard/TopLeaders.tsx
+++ components/leaderboard/TopLeaders.tsx
@@ -1,6 +1,13 @@
+import { useSetRecoilState } from 'recoil';
+
+import { modalPartsState, openModalState } from 'recoils/modal';
+
 import { TopRanker } from 'types/rankTypes';
 
 import useCustomQuery from 'hooks/useCustomQuery';
+
+import ProfileButtons from 'components/global/buttons/buttonContainers/ProfileButtons';
+import Profile from 'components/myPage/Profile';
 
 import styles from 'styles/leaderboard/TopLeaders.module.scss';
 
@@ -9,6 +16,8 @@ type TopLeadersProps = {
 };
 
 export default function TopLeaders({ topLeaderCount }: TopLeadersProps) {
+  const setModalParts = useSetRecoilState(modalPartsState);
+  const setOpenModal = useSetRecoilState(openModalState);
   const { get } = useCustomQuery();
   const { data, isLoading, isError } = get(
     ['topRank_key'],
@@ -29,8 +38,14 @@ export default function TopLeaders({ topLeaderCount }: TopLeadersProps) {
     },
   ];
 
-  const handleNicknameClick = () => {
-    // 프로필 모달 띄우기
+  const handleNicknameClick = (e: React.MouseEvent<HTMLElement>) => {
+    const nickname = (e.target as HTMLElement).innerHTML;
+    setModalParts({
+      head: null,
+      body: <Profile userName={nickname} />,
+      tail: <ProfileButtons target={nickname} />,
+    });
+    setOpenModal(true);
   };
 
   if (isLoading) return null;
