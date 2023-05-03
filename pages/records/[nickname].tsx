8073f879b6b64cd8fc32d5551e58137105e31f31
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-03 14:19:58 +0900

    [refactor] searchbar 분리

--- pages/records/[nickname].tsx
+++ pages/records/[nickname].tsx
@@ -7,38 +7,21 @@ import React, { useState } from 'react';
 import PageHeader from 'components/global/PageHeader';
 import MatchHistory from 'components/records/MatchHistory';
 
-import styles from 'styles/matchHistory/MatchHistory.module.scss';
+import styles from 'styles/records/Records.module.scss';
+
+import SearchBar from '../../components/global/SearchBar';
 
 export default function MatchHistoryPage() {
   const router = useRouter();
   const { t } = useTranslation('records');
-  const query = router.query.nickname as string; // TODO: nickname error handle when undefined or arr
-  const [searchQuery, setSearchQuery] = useState(query);
-  const [nickname, setNickname] = useState(query);
-  function handleSearch(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    setNickname(searchQuery);
-    const newQuery = encodeURIComponent(searchQuery.trim());
-    router.push(`/records/${newQuery}/`);
-  }
-
-  function handleSearchQueryChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setSearchQuery(event.target.value);
-  }
+  const defaultNickname = router.query.nickname as string; // TODO: nickname error handle when undefined or arr
+  const [nickname, setNickname] = useState(defaultNickname);
 
   return (
     <div className={styles.matchHistoryPageContainer}>
       <PageHeader title={t('Match History')} button={null} />
-      <form onSubmit={handleSearch} className={styles.searchBar}>
-        <input
-          type='text'
-          value={searchQuery}
-          onChange={handleSearchQueryChange}
-          placeholder={'Search by nickname'}
-        />
-        <button type='submit'>{'Search'}</button>
-      </form>
-      <MatchHistory key={nickname} query={nickname} />
+      <SearchBar onSubmit={setNickname} initValue={defaultNickname} />
+      <MatchHistory key={nickname} nickname={nickname} />
     </div>
   );
 }
