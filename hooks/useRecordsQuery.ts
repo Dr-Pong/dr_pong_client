a4c1a1e3c7f70ed715a2937bb6b8f2e9135d4fd3
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-03 14:26:18 +0900

    [fix] userName X nickname으로 통일

--- hooks/useRecordsQuery.ts
+++ hooks/useRecordsQuery.ts
@@ -4,18 +4,18 @@ import useCustomQuery from 'hooks/useCustomQuery';
 
 import instance from 'utils/axios';
 
-const useRecordsQuery = (userName: string) => {
+const useRecordsQuery = (nickname: string) => {
   const { get } = useCustomQuery();
   const fetchMatchHistory = async (lastGameId: number): Promise<Records> => {
     const count = 10;
     return (
       await instance.get(
-        `/users/${userName}/records?lastGameId=${lastGameId}&count=${count}`
+        `/users/${nickname}/records?lastGameId=${lastGameId}&count=${count}`
       )
     ).data;
   };
   const getMatchDetail = (gameId: number) => {
-    return get([`matchDetail`, gameId], `/users/${userName}/records/${gameId}`);
+    return get([`matchDetail`, gameId], `/users/${nickname}/records/${gameId}`);
   };
   return { fetchMatchHistory, getMatchDetail };
 };
