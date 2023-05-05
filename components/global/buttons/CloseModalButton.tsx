7f869bb88540fa7dd706969f8e686253a426101e
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-05 17:22:58 +0900

    [refactor] Close 모달 버튼의 정체성이란..? 이제 정해짐

--- components/global/buttons/CloseModalButton.tsx
+++ components/global/buttons/CloseModalButton.tsx
@@ -1,17 +1,15 @@
-import { useResetRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
-import { modalPartsState, openModalState } from 'recoils/modal';
+import { openModalState } from 'recoils/modal';
 
 import { ButtonProps } from 'types/buttonTypes';
 
 import styles from 'styles/global/Button.module.scss';
 
-export default function ModalButton({ style, color, children }: ButtonProps) {
+export default function CloseModalButton({ style, color, children }: ButtonProps) {
   const setOpenModal = useSetRecoilState(openModalState);
-  const resetModalParts = useResetRecoilState(modalPartsState);
   const handleModalClose = () => {
     setOpenModal(false);
-    resetModalParts();
   };
 
   return (
