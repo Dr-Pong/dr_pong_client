d0030a518b8f39edaaa66b433016a14d33293222
tamagoyakii <rlawlgus4063@naver.com>
2023-05-05 16:28:56 +0900

    [reefactor] login 체크하는 역할의 컴포넌트 분리 #102

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,17 +1,15 @@
 import { LayoutProps } from 'pages/_app';
 
-import NavigationBar from 'components/layouts/NavigationBar';
 import Modal from 'components/modals/Modal';
 
 import styles from 'styles/layouts/Layout.module.scss';
 
-export default function NavigationLayout({ children }: LayoutProps) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <div id='layoutRoot'>
       <Modal />
       <div className={styles.layoutContainer}>
         <div className={styles.pageContainer}>{children}</div>
-        <NavigationBar />
       </div>
     </div>
   );
