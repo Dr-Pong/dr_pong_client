8c424a8bae7e65df812f529e97576499fc72dfdb
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-05 17:23:47 +0900

    [feat] 기본적으로 handler를 실행하고 모달을 닫아주는 ModalButton 신설

---
+++ components/global/buttons/ModalButton.tsx
@@ -0,0 +1,27 @@
+import { useSetRecoilState } from 'recoil';
+
+import { openModalState } from 'recoils/modal';
+
+import { ButtonProps } from 'types/buttonTypes';
+
+import styles from 'styles/global/Button.module.scss';
+
+export default function ModalButton({
+  style,
+  color,
+  handleButtonClick,
+  children,
+}: ButtonProps) {
+  const setOpenModal = useSetRecoilState(openModalState);
+  const handleButtonClickAndClose = (e: React.FormEvent<HTMLFormElement>) => {
+    handleButtonClick?.(e);
+    setOpenModal(false);
+  };
+  return (
+    <form onClick={handleButtonClickAndClose}>
+      <button className={`${styles[style]} ${styles[color]}`} type='button'>
+        {children}
+      </button>
+    </form>
+  );
+}
