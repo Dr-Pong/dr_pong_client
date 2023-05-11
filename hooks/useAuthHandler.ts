4c5e442b16f7f6f010f203ff0fca24307229367f
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-11 20:06:34 +0900

    [feat] 인증 관련된 부분에서 겹치는 요소들을 훅으로 뺍니다 #134

---
+++ hooks/useAuthHandler.ts
@@ -0,0 +1,33 @@
+import { AxiosResponse } from 'axios';
+import { useSetRecoilState } from 'recoil';
+
+import { useRouter } from 'next/router';
+
+import { useCookies } from 'react-cookie';
+
+import { loginState } from 'recoils/login';
+
+const useAuthHandler = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['Authorization']);
+  const setLogin = useSetRecoilState(loginState);
+  const router = useRouter();
+
+  const onAuthSuccess = (res: AxiosResponse) => {
+    const { token } = res.data;
+    setCookie('Authorization', `Bearer ${token}`, {
+      path: '/',
+      httpOnly: true,
+    });
+    setLogin(true);
+    router.push('/');
+  };
+
+  const onAuthFailure = () => {
+    removeCookie('Authorization', { path: '/' });
+    setLogin(false);
+    router.push('/login');
+  };
+  return { onAuthSuccess, onAuthFailure };
+};
+
+export default useAuthHandler;
