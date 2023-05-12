34fcafe878b50758e14deeb5f700098497b157d4
tamagoyakii <rlawlgus4063@naver.com>
2023-05-12 16:59:10 +0900

    [feat] 로그아웃 구현 #42

--- components/settings/SettingsFrame.tsx
+++ components/settings/SettingsFrame.tsx
@@ -1,11 +1,12 @@
 import useTranslation from 'next-translate/useTranslation';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
 
 import { useRouter } from 'next/router';
 
 import { useCookies } from 'react-cookie';
 
 import { loginState } from 'recoils/login';
+import { userState } from 'recoils/user';
 
 import BasicButton from 'components/global/buttons/BasicButton';
 import LocaleField from 'components/settings/LocaleField';
@@ -15,6 +16,9 @@ import styles from 'styles/settings/Settings.module.scss';
 
 export default function SettingsFrame() {
   const { t } = useTranslation('settings');
+  const [cookies, setCookie, removeCookie] = useCookies(['Authorization']);
+  const setLogin = useSetRecoilState(loginState);
+  const resetUserState = useResetRecoilState(userState);
   const router = useRouter();
 
   const settingFields = [
@@ -29,11 +33,9 @@ export default function SettingsFrame() {
   ];
 
   const handleLogout = () => {
-    const [cookies, setCookie, removeCookie] = useCookies(['Authorization']);
-    const setLogin = useSetRecoilState(loginState);
-
     removeCookie('Authorization');
     setLogin(false);
+    resetUserState();
     router.push('/');
   };
 
