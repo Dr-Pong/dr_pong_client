25250082fea2fd9830deded53c1bac3df1bcf7d1
tamagoyakii <rlawlgus4063@naver.com>
2023-05-12 15:07:19 +0900

    [fix] 로그인/로그아웃 버튼 조건 수정 #42

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,6 +39,11 @@ export default function Home() {
             </div>
           );
         })}
+        {!login && (
+          <div className={styles.pageLink}>
+            <Link href='/login'>{t('Login')}</Link>
+          </div>
+        )}
       </div>
     </div>
   );
