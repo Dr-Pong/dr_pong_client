b27cedf1fb893cf3bd681d22a2422530cdff123a
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-12 13:34:05 +0900

    [feat] authentication 페이지에 잘못 접근하는 경우 /로 push #133

--- pages/authentication.tsx
+++ pages/authentication.tsx
@@ -1,6 +1,11 @@
 import useTranslation from 'next-translate/useTranslation';
+import { useRecoilValue } from 'recoil';
+
+import { useRouter } from 'next/router';
 
 import { ReactElement } from 'react';
+
+import { userState } from 'recoils/user';
 
 import AuthenticationFrame from 'components/authentication/AuthenticationFrame';
 import PageHeader from 'components/global/PageHeader';
@@ -11,7 +16,11 @@ import styles from 'styles/authentication/Authentication.module.scss';
 
 export default function Authentication() {
   const { t } = useTranslation('authentication');
-
+  const { tfaRequired } = useRecoilValue(userState);
+  const router = useRouter();
+  if (!tfaRequired) {
+    router.push('/');
+  }
   return (
     <div className={styles.AuthenticationPageContainer}>
       <PageHeader title={t('2fa')} />
