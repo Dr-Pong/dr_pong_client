d6fb5d1639c5d0e87b18ddf7d0ab505415a1079e
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-12 15:02:14 +0900

    [feat] validation fix && 키보드로 이동 가능

--- components/authentication/NumberInputBox.tsx
+++ components/authentication/NumberInputBox.tsx
@@ -11,16 +11,40 @@ export default function NumberInputBox({
 }) {
   const boxes: number[] = Array.from({ length: boxNumber }, (_, i) => i);
 
-  const handleFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const currentValue = e.target.value;
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.value = validateInput(e.target.value);
+  };
+
+  const handleFocusJump = (e: React.ChangeEvent<HTMLInputElement>) => {
     const currentIdx = parseInt(e.target.id);
-    const regex = /^[0-9\b -]$/;
-    if (!regex.test(e.target.value)) {
-      e.target.value = e.target.value.slice(-1);
-    }
-    if (currentValue && currentIdx < boxNumber - 1) {
+    if (e.target.value && currentIdx < boxNumber - 1) {
       inputRef.current[currentIdx + 1].focus();
     }
+  };
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    handleInputChange(e);
+    handleFocusJump(e);
+  };
+  const handleFocusOnLast = (target: HTMLInputElement) => {
+    const temp = target.value;
+    target.value = '';
+    target.value = temp;
+  };
+  const handleClickFocus = (e: React.MouseEvent<HTMLInputElement>) => {
+    handleFocusOnLast(e.target as HTMLInputElement);
+  };
+  const handleArrowKey = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const current = e.target as HTMLInputElement;
+    let idx = parseInt(current.id);
+    if (e.key === 'ArrowLeft' && idx > 0) {
+      idx -= 1;
+    }
+    if (e.key === 'ArrowRight' && idx < boxNumber - 1) {
+      idx += 1;
+    }
+    const targetInputBox = inputRef.current[idx];
+    targetInputBox.focus();
+    handleFocusOnLast(targetInputBox);
   };
 
   return (
@@ -33,7 +57,9 @@ export default function NumberInputBox({
               id={`${idx}`}
               className={styles.inputBox}
               ref={(el) => (inputRef.current[idx] = el)}
-              onChange={handleFocus}
+              onChange={handleOnChange}
+              onClick={handleClickFocus}
+              onKeyDown={handleArrowKey}
             />
           </span>
         );
@@ -41,3 +67,14 @@ export default function NumberInputBox({
     </div>
   );
 }
+
+const validateInput = (value: string): string => {
+  const regex = /^[0-9\b -]$/;
+  if (!regex.test(value)) {
+    value = value.slice(-1);
+  }
+  if (!regex.test(value)) {
+    return '';
+  }
+  return value;
+};
