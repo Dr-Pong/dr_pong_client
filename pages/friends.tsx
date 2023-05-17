5089cff3dc498db1f4604dfc4d2fa7aa8e0627e7
triplecheeseburger <hangyulkim94@gmail.com>
2023-05-17 13:22:38 +0900

    [feat] friendPageFrame 제거하고 페이지에 넣음

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -1,9 +1,15 @@
 import useTranslation from 'next-translate/useTranslation';
+import { useRecoilState } from 'recoil';
 
 import React, { ReactElement } from 'react';
 
-import FriendsPageFrame from 'components/friends/FriendsPageFrame';
+import { friendsTabState } from 'recoils/friends';
+
+import { FriendTab } from 'types/friendTypes';
+
+import FriendTabContents from 'components/friends/FriendTabContents';
 import PageHeader from 'components/global/PageHeader';
+import TabsViewProvider from 'components/global/TabsViewProvider';
 import LoginFilter from 'components/layouts/LoginFilter';
 import NavigationLayout from 'components/layouts/NavigationLayout';
 
@@ -11,10 +17,26 @@ import styles from 'styles/friends/Friends.module.scss';
 
 export default function Friends() {
   const { t } = useTranslation('friends');
+
+  const [tab, setTab] = useRecoilState(friendsTabState);
+
+  const handleTabClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const div = event.target as HTMLDivElement;
+    setTab(div.id as FriendTab);
+  };
+
   return (
     <div className={styles.friendsPageContainer}>
       <PageHeader title={t('Friends')} />
-      <FriendsPageFrame />
+      <div className={styles.friendsPageFrame}>
+        <TabsViewProvider
+          namespace={'friends'}
+          tabNames={['friend', 'request', 'block']}
+          handleTabClick={handleTabClick}
+        >
+          <FriendTabContents key={tab} tab={tab} />
+        </TabsViewProvider>
+      </div>
     </div>
   );
 }
