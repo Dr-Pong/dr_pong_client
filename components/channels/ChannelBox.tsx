6692a25826f8e6a7056ad9d4959f6ca591ac4425
keonwoo98 <keonwoo98@naver.com>
2023-05-19 12:31:03 +0900

    [refactor] any는 쓰지 않도록 해요

--- components/channels/ChannelBox.tsx
+++ components/channels/ChannelBox.tsx
@@ -1,18 +1,23 @@
 import { AllChannels } from 'types/channelTypes';
 
+import { EachChannel } from 'types/channelTypes';
+
 import styles from 'styles/channels/ChannelBox.module.scss';
 
 export default function ChannelBox({ channel }: { channel: AllChannels | undefined }) {
+  const onClickJoinChannel = () => {
+  };
+
   return (
     <div className={styles.channelList}>
-      {channel?.channel.map(({ id, title, access, headCount, maxCount }: any) => {
+      {channel?.channel.map(({ id, title, access, headCount, maxCount }: EachChannel) => {
         return (
           <div
             className={styles.channelBox}
             data-id={id}
             data-status={access}
             key={id}
-            onClick={() => { }}
+            onClick={onClickJoinChannel}
           >
             <div>
               {title.length > 20
