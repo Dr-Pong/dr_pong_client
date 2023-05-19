a8875d5d3e82969a300d594353d66cdb51516dd4
keonwoo98 <keonwoo98@naver.com>
2023-05-20 00:27:29 +0900

    [feat] fetch로 받아오던 채널 리스트 query hook으로 로직 변경

--- pages/channels.tsx
+++ pages/channels.tsx
@@ -2,6 +2,8 @@ import useTranslation from 'next-translate/useTranslation';
 
 import { useState, useEffect, ReactElement } from 'react';
 
+import useCustomQuery from 'hooks/useCustomQuery';
+
 import PageHeader from 'components/global/PageHeader';
 import LoginFilter from 'components/layouts/LoginFilter';
 import NavigationLayout from 'components/layouts/NavigationLayout';
@@ -10,7 +12,6 @@ import ChannelSearch from 'components/channels/ChannelSearch';
 import ChannelsList from 'components/channels/ChannelsList';
 
 import { AllChannels } from 'types/channelTypes';
-
 
 import styles from 'styles/channels/Channels.module.scss';
 
@@ -21,18 +22,33 @@ export default function Channels() {
   const [page, setPage] = useState(1);
   const [order, setOrder] = useState<string>('recent');
   const [keyword, setKeyword] = useState<string>('');
+  const { get } = useCustomQuery();
+  const [url, setUrl] = useState<string>(`/channels?page=${page}&count=${count}&order=${order}&keyword=${keyword}`);
+  const { data, isLoading } = get(
+    ['channels_key', url],
+    url,
+    setChannel
+  );
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/channels?page=${page}&count=${count}&order=${order}&keyword=${keyword}`)
-      .then((res) => res.json())
-      .then((data) => setChannel(data));
+    // fetch(`http://localhost:3000/api/channels?page=${page}&count=${count}&order=${order}&keyword=${keyword}`)
+    //   .then((res) => res.json())
+    //   .then((data) => setChannel(data));
+    setUrl(`/channels?page=${page}&count=${count}&order=${order}&keyword=${keyword}`);
   }, [page, order, keyword]);
+
+  useEffect(() => {
+    setUrl(`/channels?page=${1}&count=${count}&order=${order}&keyword=${keyword}`);
+  }, [keyword]);
+
+  if (isLoading) return null;
 
   return (
     <div className={styles.channelsPageContainer}>
       <PageHeader title={t('Channels')} />
       <div className={styles.channelLayout}>
         <ChannelSetting
+          order={order}
           setOrder={setOrder}
         />
         <ChannelSearch
