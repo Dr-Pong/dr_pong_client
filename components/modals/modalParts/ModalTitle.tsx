e761c98133d49e303dce78f926c8ab756f6f72e6
keonwoo98 <keonwoo98@naver.com>
2023-05-25 12:53:32 +0900

    [feat] MotalTitle에 closeBtn을 prop으로 주면 닫기 버튼 생성 #162

--- components/modals/modalParts/ModalTitle.tsx
+++ components/modals/modalParts/ModalTitle.tsx
@@ -1,9 +1,30 @@
+import { useSetRecoilState } from 'recoil';
+import { openModalState } from 'recoils/modal';
+
+import { IoIosClose } from 'react-icons/io';
+
 import styles from 'styles/modals/Modal.module.scss';
 
 type ModalTitleProps = {
   title: string;
+  closeBtn?: boolean;
 };
 
-export default function ModalTitle({ title }: ModalTitleProps) {
-  return <div className={styles.modalTitle}>{title}</div>;
+export default function ModalTitle({ title, closeBtn }: ModalTitleProps) {
+  const setOpenModal = useSetRecoilState(openModalState);
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
+  return (
+    <div className={styles.modalTitle}>
+      <div>{title}</div>
+      {closeBtn && (
+        <IoIosClose
+          style={{ cursor: 'pointer', fontSize: '3rem' }}
+          onClick={handleModalClose}
+        />
+      )}
+    </div>
+  );
 }
