b848e89ff6104c985c1d9ff1a316f3ec9408927d
tamagoyakii <rlawlgus4063@naver.com>
2023-05-25 15:44:14 +0900

    [feat] dm/noti 버튼 클릭 이벤틑 추가 #169

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,22 +1,39 @@
+import { useState } from 'react';
 import { RiSendPlaneFill, RiSettings4Fill } from 'react-icons/ri';
 import { TbBellFilled } from 'react-icons/tb';
 
 import useModalProvider from 'hooks/useModalProvider';
 
+import SideBar from 'components/layouts/SideBar';
+
 import styles from 'styles/layouts/Header.module.scss';
 
 export default function Header() {
   const { useSettingsModal } = useModalProvider();
+  const [sideBarType, setSideBarType] = useState<string | null>(null);
 
   return (
-    <div className={styles.headerContainer}>
-      <RiSettings4Fill
-        className={styles.headerIcon}
-        onClick={useSettingsModal}
-      />
-      <div>
-        <RiSendPlaneFill className={styles.headerIcon} />
-        <TbBellFilled className={styles.headerIcon} />
+    <div>
+      <SideBar type={sideBarType} setType={setSideBarType} />
+      <div className={styles.headerContainer}>
+        <RiSettings4Fill
+          className={styles.headerIcon}
+          onClick={useSettingsModal}
+        />
+        <div>
+          <RiSendPlaneFill
+            className={styles.headerIcon}
+            onClick={() => {
+              setSideBarType('messages');
+            }}
+          />
+          <TbBellFilled
+            className={styles.headerIcon}
+            onClick={() => {
+              setSideBarType('notifications');
+            }}
+          />
+        </div>
       </div>
     </div>
   );
