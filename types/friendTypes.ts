0b08d853842520e17452a04205398d1be52ffa37
tamagoyakii <rlawlgus4063@naver.com>
2023-05-25 15:43:14 +0900

    [feat] chat 타입 추가 #169

--- types/friendTypes.ts
+++ types/friendTypes.ts
@@ -1,11 +1,19 @@
 export type Activity = 'online' | 'offline' | 'inGame';
 export type FriendTab = 'all' | 'pending' | 'block';
 export type SearchUser = 'find';
+
 export type Friend = {
   nickname: string;
   imgUrl: string;
   status?: Activity;
 };
+
 export type UserListResponse = {
   users: Friend[];
+};
+
+export type Chat = {
+  nickname: string;
+  imgUrl: string;
+  hasNewChat: boolean;
 };
