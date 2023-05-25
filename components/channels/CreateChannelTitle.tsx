c5a9bd054484456dd449672a72221f4fb2ae13b1
keonwoo98 <keonwoo98@naver.com>
2023-05-25 18:05:51 +0900

    [fix] titleCheck props 제거

--- components/channels/CreateChannelTitle.tsx
+++ components/channels/CreateChannelTitle.tsx
@@ -4,11 +4,9 @@ import styles from 'styles/channels/CreateChannel.module.scss';
 
 export default function CreateChannelTitle({
   title,
-  titleCheck,
   handleTitleChange
 }: {
   title: string,
-  titleCheck: boolean,
   handleTitleChange: (event: ChangeEvent<HTMLInputElement>) => void
 }) {
   return (
@@ -20,7 +18,7 @@ export default function CreateChannelTitle({
         onChange={handleTitleChange}
         placeholder={'Channel title'}
       />
-      <div className={styles.info}>* 16글자 이내 {titleCheck && '* Title 필수임'}</div>
+      <div className={styles.info}>* 16글자 이내</div>
     </div>
   );
 }
