2856418603150d21aaee19209de13286391baaab
keonwoo98 <keonwoo98@naver.com>
2023-05-25 12:47:37 +0900

    [fix] ChannelSearch를 ChannelSetting 컴포넌트로 넣어서 하나로 합치기 #162

--- components/channels/ChannelSetting.tsx
+++ components/channels/ChannelSetting.tsx
@@ -1,27 +1,35 @@
-import { Dispatch, SetStateAction } from 'react';
+import { useState, useCallback, Dispatch, SetStateAction, FormEvent, ChangeEvent } from 'react';
 
 import ChannelDropdown from 'components/channels/ChannelDropdown';
 
-import { QueryClient } from 'react-query';
+import useModalProvider from 'hooks/useModalProvider';
 
 import styles from 'styles/channels/ChannelSetting.module.scss';
 import { IoIosAdd } from 'react-icons/io';
-import { IoIosRefresh } from 'react-icons/io';
+import { IoIosSearch } from 'react-icons/io';
 
 import BasicButton from 'components/global/buttons/BasicButton';
 
 export default function ChannelSetting({
   order,
   setOrder,
-  queryClient
+  setKeyword
 }: {
   order: string,
   setOrder: Dispatch<SetStateAction<string>>,
-  queryClient: QueryClient
+  setKeyword: Dispatch<SetStateAction<string>>
 }) {
-  const RefreshChannel = () => {
-    queryClient.invalidateQueries({ queryKey: ['channel_key'] });
-  }
+  const { useCreateChannelModal } = useModalProvider();
+  const [channelTitle, setChannelTitle] = useState<string>('');
+  const handleKeywordSubmit = useCallback((event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setKeyword(channelTitle);
+  }, [channelTitle]);
+
+  const handleKeywordChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setChannelTitle(event.target.value);
+  }, [channelTitle]);
+
   const ChannelCreate = () => {
     // 변경된 useMutation 적용 예정
     // 모달 띄우고 title, password, maxCount 설정
@@ -30,24 +38,36 @@ export default function ChannelSetting({
 
   return (
     <div className={styles.channelSetting}>
+      <form className={styles.channelSearch} onSubmit={handleKeywordSubmit}>
+        <input
+          type='text'
+          value={channelTitle}
+          onChange={handleKeywordChange}
+          placeholder='Search channel'
+          style={{
+            color: 'white',
+            background: 'transparent'
+          }}
+        />
+        <BasicButton
+          style={'small'}
+          color={'opaque'}
+          handleButtonClick={handleKeywordSubmit}
+        >
+          <IoIosSearch />
+        </BasicButton>
+      </form>
+      <BasicButton
+        style={'small'}
+        color={'opaque'}
+        handleButtonClick={useCreateChannelModal}
+      >
+        <IoIosAdd />
+      </BasicButton>
       <ChannelDropdown
         order={order}
         setOrder={setOrder}
       />
-      <BasicButton
-        style={'short'}
-        color={'opaque'}
-        handleButtonClick={RefreshChannel}
-      >
-        <IoIosRefresh />
-      </BasicButton>
-      <BasicButton
-        style={'short'}
-        color={'opaque'}
-        handleButtonClick={ChannelCreate}
-      >
-        <IoIosAdd />
-      </BasicButton>
     </div>
   );
 }
