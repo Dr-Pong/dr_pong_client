fedaca6da94db75f08c89ef9ca5fcd9ce9798f58
tamagoyakii <rlawlgus4063@naver.com>
2023-05-28 22:10:09 +0900

    [feat] api 추가 #178

---
+++ pages/api/users/notifications/games/[id].ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+type Error = {
+  message: string;
+};
+
+export default (req: NextApiRequest, res: NextApiResponse<Error>) => {
+  if (req.method === 'DELETE') {
+    res.status(200).json({ message: 'success' });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
