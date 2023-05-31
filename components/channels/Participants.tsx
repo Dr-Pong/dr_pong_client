9d5d38355bc9d12b956a62ee5c0b9462cb359a60
keonwoo98 <keonwoo98@naver.com>
2023-05-31 14:33:14 +0900

    [feat] 채널 참여자 목록 사이드바 구현 #174

---
+++ components/channels/Participants.tsx
@@ -0,0 +1,88 @@
+import { useRouter } from 'next/router';
+
+import { useState } from 'react';
+
+import useChatQuery from 'hooks/useChatQuery';
+
+import useModalProvider from 'hooks/useModalProvider';
+
+import { ChattingType, Participant, ParticipantsResponse, UserImageMap } from 'types/chatTypes';
+
+import { FaCrown, FaBan } from 'react-icons/fa';
+import { RiVipCrownLine } from 'react-icons/ri';
+import { GiHighKick } from 'react-icons/gi';
+import { BsFillVolumeUpFill, BsVolumeMuteFill } from 'react-icons/bs';
+import { FiUserPlus } from 'react-icons/fi';
+import { MdLogout } from 'react-icons/md';
+
+import styles from 'styles/channels/Participants.module.scss';
+
+export default function Participants() {
+  const router = useRouter();
+  const { roomType, roomId } = router.query;
+  const [participants, setParticipants] = useState<ParticipantsResponse>();
+  const [userImageMap, setUserImageMap] = useState<UserImageMap>({});
+  const { useProfileModal } = useModalProvider();
+
+  const { getChatUsers } = useChatQuery(
+    roomType as ChattingType,
+    roomId as string
+  );
+
+  const { data, isLoading, isError } = getChatUsers(setUserImageMap);
+  if (isLoading) return null;
+  if (isError) return null;
+
+  const isOwner = data.me.roleType === 'owner';
+  const isAdmin = data.me.roleType === 'admin';
+
+  const renderButtons = (participant: Participant) => {
+    if (isOwner || (isAdmin && participant.roleType === 'normal')) {
+      return (
+        <div className={styles.buttons}>
+          {isOwner && <button><RiVipCrownLine /></button>}
+          <button><GiHighKick /></button>
+          <button><FaBan /></button>
+          <button>{participant.isMuted ? <BsVolumeMuteFill /> : <BsFillVolumeUpFill />}</button>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const handleParticipantClick = (e: React.MouseEvent<HTMLElement>) => {
+    const target = e.target as HTMLElement;
+    const nickname = target.querySelector('span')?.innerHTML;
+    if (nickname) {
+      useProfileModal(nickname);
+    }
+  };
+
+
+  return (
+    <div className={styles.participantsContainer}>
+      <div className={styles.participant} onClick={handleParticipantClick}>
+        <img src={data.me.imgUrl} className={styles.profileImage} />
+        <div>
+          <span>{data.me.nickname}</span>
+          {isOwner ? <FaCrown /> : isAdmin ? <RiVipCrownLine /> : null}
+        </div>
+      </div>
+
+      {data.participants.map((participant: Participant) => (
+        <div key={participant.nickname} className={styles.participant} onClick={handleParticipantClick}>
+          <img src={participant.imgUrl} className={styles.profileImage} />
+          <div>
+            <span>{participant.nickname}</span>
+            {participant.roleType === 'owner' ? <FaCrown /> : participant.roleType === 'admin' ? <RiVipCrownLine /> : null}
+            {renderButtons(participant)}
+          </div>
+        </div>
+      ))}
+      <div className={styles.footer}>
+        <button>초대하기 <FiUserPlus /></button>
+        <button>나가기 <MdLogout /></button>
+      </div>
+    </div>
+  );
+}
