39982fd13bbd46bd1f3d4113ab31fdac5ae9d44e
tamagoyakii <rlawlgus4063@naver.com>
2023-06-01 14:38:57 +0900

    [fix] tabProvider 인자 추가 #144

--- pages/friends.tsx
+++ pages/friends.tsx
@@ -33,6 +33,7 @@ export default function Friends() {
         <TabProvider
           namespace={'friends'}
           tabNames={['all', 'pending', 'block']}
+          currentTab={tab}
           handleTabClick={handleTabClick}
         >
           <FriendTabContents key={tab} tab={tab} />
