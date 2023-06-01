a6a5ad6b6e73e3d292c5f0da3f16022a0d667288
triplecheeseburger <hangyulkim94@gmail.com>
2023-06-01 14:59:16 +0900

    [feat] 채팅 전송 실패시 refresh, delete 가능하게 만들기 #189

--- components/chats/ChatInputBox.tsx
+++ components/chats/ChatInputBox.tsx
@@ -4,38 +4,98 @@ import React, {
   useCallback,
   useState,
 } from 'react';
+import { IoIosClose, IoIosRefresh } from 'react-icons/io';
 import { RiSendPlaneFill } from 'react-icons/ri';
 
-import { ChatBoxProps } from 'types/chatTypes';
+import { ChatBoxProps, ChattingType } from 'types/chatTypes';
+
+import useChatQuery from 'hooks/useChatQuery';
+
+import BasicButton from 'components/global/buttons/BasicButton';
 
 import styles from 'styles/chats/ChatInputBox.module.scss';
 
 export default function ChatInputBox({
+  roomType,
   roomId,
   setChatBoxes,
   topRef,
 }: {
+  roomType: ChattingType;
   roomId: string;
   setChatBoxes: React.Dispatch<SetStateAction<ChatBoxProps[]>>;
   topRef: React.RefObject<HTMLDivElement>;
 }) {
+  const { mutate } = useChatQuery(roomType, roomId).chatMutationPost();
   const [message, setMessage] = useState<string>('');
+
+  function onSuccess() {
+    setChatBoxes((previous) => {
+      return [
+        {
+          id: previous[0].id + 1,
+          message,
+          time: new Date(),
+        },
+        ...previous,
+      ];
+    });
+  }
+
+  function onErrorHandler(e: React.FormEvent | React.MouseEvent) {
+    setChatBoxes((previous) => {
+      const newId = previous[0].id + 1;
+      const handleChatDelete = () => {
+        setChatBoxes((previous) => {
+          return previous.filter((chat) => chat.id !== newId);
+        });
+      };
+      const handleChatRefresh = () => {
+        setChatBoxes((previous) => {
+          return previous.filter((chat) => chat.id !== newId);
+        });
+        handleChatSubmit(e);
+      };
+      const buttons = [
+        <BasicButton
+          key={'refresh'}
+          style={'square'}
+          color={'white'}
+          handleButtonClick={handleChatRefresh}
+        >
+          <IoIosRefresh />
+        </BasicButton>,
+        <BasicButton
+          key={'delete'}
+          style={'square'}
+          color={'white'}
+          handleButtonClick={handleChatDelete}
+        >
+          <IoIosClose />
+        </BasicButton>,
+      ];
+      return [
+        {
+          id: newId,
+          message,
+          buttons,
+        },
+        ...previous,
+      ];
+    });
+  }
 
   const handleChatSubmit = useCallback(
     (e: React.FormEvent | React.MouseEvent) => {
       e.preventDefault();
       if (!isMessageValid(message)) return;
-      //mutate;
-      setChatBoxes((previous) => {
-        return [
-          {
-            id: previous[0].id + 1,
-            message,
-            time: new Date(),
-          },
-          ...previous,
-        ];
-      });
+      mutate(
+        { message },
+        {
+          onSuccess: () => onSuccess(),
+          onError: () => onErrorHandler(e),
+        }
+      );
       setMessage('');
       topRef.current?.scrollIntoView({ behavior: 'auto' });
     },
