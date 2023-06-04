a29ae5fcec04055b7bfaaee2d06924558529029f
keonwoo98 <keonwoo98@naver.com>
2023-06-04 23:29:45 +0900

    [feat] channel role event 버튼들 분리해서 구현 #214

---
+++ components/channels/ChannelRoleButtons.tsx
@@ -0,0 +1,50 @@
+import useRelationButtons from 'hooks/useRelationButtons';
+
+import { Participant } from 'types/chatTypes';
+import { ButtonDesign } from 'types/buttonTypes';
+
+import { FaBan } from 'react-icons/fa';
+import { TbCrown, TbCrownOff } from 'react-icons/tb';
+import { GiHighKick } from 'react-icons/gi';
+import { BsFillVolumeUpFill, BsVolumeMuteFill } from 'react-icons/bs';
+
+import styles from 'styles/channels/ChannelRoleButtons.module.scss';
+
+const buttonDesign: ButtonDesign = {
+  style: 'transparent',
+  color: 'none',
+};
+
+export default function ChannelRoleButtons({ roomId, role, participant }: { roomId: string, role: string, participant: Participant }) {
+  const admin = `/channels/${roomId}/admin`;
+  const kick = `/channels/${roomId}/kick`;
+  const ban = `/channels/${roomId}/ban`;
+  const mute = `/channels/${roomId}/mute`;
+  const { channelRoleEvent } = useRelationButtons(buttonDesign, participant.nickname);
+  const buttons: { [key: string]: JSX.Element[]; } = {
+    owner: [
+      participant.roleType === 'admin'
+        ? channelRoleEvent(<TbCrownOff />, admin, 'delete')
+        : channelRoleEvent(<TbCrown />, admin, 'post'),
+      channelRoleEvent(<GiHighKick />, kick, 'post'),
+      channelRoleEvent(<FaBan />, ban, 'post'),
+      participant.isMuted
+        ? channelRoleEvent(<BsVolumeMuteFill />, mute, 'delete')
+        : channelRoleEvent(<BsFillVolumeUpFill />, mute, 'post'),
+    ],
+    admin: [
+      channelRoleEvent(<GiHighKick />, kick, 'post'),
+      channelRoleEvent(<FaBan />, ban, 'post'),
+      participant.isMuted
+        ? channelRoleEvent(<BsVolumeMuteFill />, mute, 'delete')
+        : channelRoleEvent(<BsFillVolumeUpFill />, mute, 'post'),
+    ],
+  };
+
+  return (
+    <div className={styles.buttons}>
+      {(role === 'owner' || (role === 'admin' && participant.roleType === 'normal')) &&
+        buttons[role].map((c) => c)}
+    </div>
+  )
+}
