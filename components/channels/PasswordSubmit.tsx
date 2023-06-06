de72bdbf8219ee0e7ee0c0074ce8edb47553f9e2
tamagoyakii <rlawlgus4063@naver.com>
2023-06-07 03:04:14 +0900

    [fix] PasswordInput 컴포넌트 재사용 #220

--- components/channels/PasswordSubmit.tsx
+++ components/channels/PasswordSubmit.tsx
@@ -1,31 +1,36 @@
+import useTranslation from 'next-translate/useTranslation';
 import { useSetRecoilState } from 'recoil';
 
 import { useRouter } from 'next/router';
 
-import { ChangeEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 import { openModalState } from 'recoils/modal';
 
+import { ChannelInfo } from 'types/channelTypes';
+
 import useCustomQuery from 'hooks/useCustomQuery';
 
+import { defaultChannelSettings } from 'components/channels/channelSettings/ChannelSettings';
+import PasswordInput from 'components/channels/channelSettings/PasswordInput';
 import BasicButton from 'components/global/buttons/BasicButton';
 
-import styles from 'styles/channels/PasswordSubmit.module.scss';
+import styles from 'styles/channels/ChannelSettings.module.scss';
 
 export default function PasswordSubmit({ roomId }: { roomId: string }) {
+  const { t } = useTranslation('channels');
   const router = useRouter();
   const setOpenModal = useSetRecoilState(openModalState);
+  const [channelInfo, setChannelInfo] = useState<ChannelInfo>(
+    defaultChannelSettings
+  );
+  const { password } = channelInfo;
   const { mutationPost } = useCustomQuery();
   const { mutate } = mutationPost(`channels/${roomId}/participants`);
-  const [password, setPassword] = useState<string>('');
-  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    const regex = /^[a-zA-Z0-9]+$/;
-    if (value.length === 0 || (value.length <= 8 && regex.test(value)))
-      setPassword(value);
-  };
 
-  const handlePasswordSubmit = () => {
+  const handlePasswordSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!password) return;
     mutate(
       { password },
       {
@@ -41,26 +46,18 @@ export default function PasswordSubmit({ roomId }: { roomId: string }) {
   };
 
   return (
-    <form className={styles.modal} onSubmit={handlePasswordSubmit}>
-      <div className={styles.input}>
-        <label className={styles.label}>Password</label>
-        <input
-          className={styles.text}
-          type='text'
-          value={password}
-          onChange={handlePasswordChange}
-          placeholder='Password'
-        />
-      </div>
-      <div className={styles.button}>
-        <BasicButton
-          style='basic'
-          color='black'
-          handleButtonClick={handlePasswordSubmit}
-        >
-          Submit
-        </BasicButton>
-      </div>
+    <form className={styles.modalContainer} onSubmit={handlePasswordSubmit}>
+      <PasswordInput
+        channelInfo={channelInfo}
+        setChannelInfo={setChannelInfo}
+      />
+      <BasicButton
+        style='big'
+        color='purple'
+        handleButtonClick={handlePasswordSubmit}
+      >
+        {t('submit')}
+      </BasicButton>
     </form>
   );
 }
