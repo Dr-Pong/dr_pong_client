9625c8ceb4428c37508f2e9be6397f79360d53e9
tamagoyakii <rlawlgus4063@naver.com>
2023-06-07 02:58:09 +0900

    [fix] participants의 버튼 컴포넌트를 userBox로 확장 #220

---
+++ components/channels/participants/UserBox.tsx
@@ -0,0 +1,96 @@
+import { BsFillVolumeUpFill, BsVolumeMuteFill } from 'react-icons/bs';
+import { RiVipCrownFill, RiVipCrownLine } from 'react-icons/ri';
+import { TbBan, TbCrown, TbCrownOff, TbShoe } from 'react-icons/tb';
+
+import { ButtonDesign } from 'types/buttonTypes';
+import { Participant } from 'types/chatTypes';
+
+import useModalProvider from 'hooks/useModalProvider';
+import useRelationButtons from 'hooks/useRelationButtons';
+
+import styles from 'styles/channels/UserBox.module.scss';
+
+const buttonDesign: ButtonDesign = {
+  style: 'fit',
+  color: 'transparent',
+};
+
+type UserBoxProps = {
+  roomId?: string;
+  myRoleType?: string;
+  user: Participant;
+};
+
+export default function UserBox({ roomId, myRoleType, user }: UserBoxProps) {
+  const { nickname, imgUrl, roleType, isMuted } = user;
+  const { useProfileModal } = useModalProvider();
+  const { channelRoleEvent } = useRelationButtons(buttonDesign, nickname);
+
+  const paths = {
+    admin: `/channels/${roomId}/admin`,
+    kick: `/channels/${roomId}/kick`,
+    ban: `/channels/${roomId}/ban`,
+    mute: `/channels/${roomId}/mute`,
+  };
+
+  const roleIcons: { [key: string]: React.ReactElement | null } = {
+    owner: <RiVipCrownFill />,
+    admin: <RiVipCrownLine />,
+    normal: null,
+  };
+
+  const adminButtons = {
+    owner: <></>,
+    admin: channelRoleEvent(<TbCrownOff />, paths.admin, 'delete'),
+    normal: channelRoleEvent(<TbCrown />, paths.admin, 'post'),
+  };
+
+  const muteButtons = {
+    true: channelRoleEvent(<BsFillVolumeUpFill />, paths.mute, 'delete'),
+    false: channelRoleEvent(<BsVolumeMuteFill />, paths.mute, 'post'),
+  };
+
+  const buttons: { [key: string]: JSX.Element[] } = {
+    owner: [
+      adminButtons[roleType],
+      channelRoleEvent(<TbShoe />, paths.kick, 'post'),
+      channelRoleEvent(<TbBan />, paths.ban, 'post'),
+      muteButtons[`${isMuted}`],
+    ],
+    admin:
+      roleType === 'normal'
+        ? [
+            channelRoleEvent(<TbShoe />, paths.kick, 'post'),
+            channelRoleEvent(<TbBan />, paths.ban, 'post'),
+            muteButtons[`${isMuted}`],
+          ]
+        : [],
+    normal: [],
+  };
+
+  const handleUserClick = () => {
+    useProfileModal(nickname);
+  };
+
+  return (
+    <div
+      className={`${styles.userBoxContainer} ${!myRoleType && styles.myBox}`}
+    >
+      <img
+        src={imgUrl}
+        className={styles.profileImage}
+        onClick={handleUserClick}
+        alt='img'
+      />
+      <div className={styles.nickname}>
+        <span>{nickname}</span>
+        {roleIcons[roleType]}
+      </div>
+      {myRoleType && (
+        <div className={styles.buttons}>
+          {buttons[myRoleType].map((c) => c)}
+        </div>
+      )}
+    </div>
+  );
+}
