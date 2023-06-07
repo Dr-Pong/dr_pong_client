5ec7e6136fa9399af7d405238e0782aea5e40361
keonwoo98 <keonwoo98@naver.com>
2023-06-07 20:30:22 +0900

    [feat] data fetching loading component #227

---
+++ components/global/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from 'styles/global/LoadingSpinner.module.scss';
+
+export default function LoadingSpinner() {
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.loadingSpinner} />
+    </div>
+  );
+};
