5b2b074eec39392bf1db986b4e79eaef2b786a76
tamagoyakii <rlawlgus4063@naver.com>
2023-06-12 18:22:09 +0900

    [fix] searchbar 재사용 및 friendbox에 타입 추가 #243

--- components/friends/SearchUser.tsx
+++ components/friends/SearchUser.tsx
@@ -1,41 +1,50 @@
+import useTranslation from 'next-translate/useTranslation';
+
 import React, { useState } from 'react';
+
+import { DetailDto } from 'types/userTypes';
 
 import instance from 'utils/axios';
 
 import FriendBox from 'components/friends/FriendBox';
-import { DetailDto } from 'types/userTypes';
+import SearchBar from 'components/global/SearchBar';
+import BasicButton from 'components/global/buttons/BasicButton';
 
-import styles from 'styles/global/SearchBar.module.scss';
+import styles from 'styles/friends/SearchUser.module.scss';
 
 export default function SearchUser() {
+  const { t } = useTranslation('friends');
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [nickname, setNickname] = useState<string>('');
   const [detailDto, setDetailDto] = useState<DetailDto | null>(null);
-  const handleOnSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+
+  const handleUserSearch = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = (await instance.get(`/users/${searchQuery}/detail`)).data;
     setDetailDto(data);
     if (data) setNickname(searchQuery);
   };
 
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
-  };
-
   return (
-    <div className={styles.searchUser}>
-      <form className={styles.searchBar} onSubmit={handleOnSubmit}>
-        <input
-          type='text'
-          value={searchQuery}
-          onChange={handleOnChange}
-          placeholder={'Search by nickname'}
+    <div className={styles.searchUserContainer}>
+      <div className={styles.searchBar}>
+        <SearchBar
+          searchKey={searchQuery}
+          setSearchKey={setSearchQuery}
+          placeHolder={t('nickname')}
+          handleOnSubmit={handleUserSearch}
         />
-        <button type='submit'>{'Search'}</button>
-      </form>
-      <div className={styles.result}>
+        <BasicButton
+          style='short'
+          color='pink'
+          handleButtonClick={handleUserSearch}
+        >
+          {t('search')}
+        </BasicButton>
+      </div>
+      <div className={styles.results}>
         {detailDto === null ? (
-          <div className={styles.noResult}>no user found</div>
+          <div className={styles.noResult}>{t('no user')}</div>
         ) : (
           <FriendBox
             key={searchQuery}
@@ -43,6 +52,7 @@ export default function SearchUser() {
               nickname: nickname,
               imgUrl: detailDto.image.url,
             }}
+            type='find'
           />
         )}
       </div>
