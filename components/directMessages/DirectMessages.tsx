f94f9dff00c0bb489c3e415399bf4e3c73adb95f
tamagoyakii <rlawlgus4063@naver.com>
2023-06-19 16:27:11 +0900

    [feat] dm방 삭제 버튼 및 api 추가 #272

--- components/directMessages/DirectMessages.tsx
+++ components/directMessages/DirectMessages.tsx
@@ -1,16 +1,21 @@
+import useTranslation from 'next-translate/useTranslation';
+
 import { useState } from 'react';
 
 import { ChatList, DMRoom } from 'types/notificationTypes';
 
 import useCustomQuery from 'hooks/useCustomQuery';
 
-import LoadingSpinner from 'components/global/LoadingSpinner';
-import ErrorRefresher from 'components/global/ErrorRefresher';
 import DirectMessageBox from 'components/directMessages/DirectMessageBox';
+import ErrorRefresher from 'components/global/ErrorRefresher';
+import LoadingSpinner from 'components/global/LoadingSpinner';
+import BasicButton from 'components/global/buttons/BasicButton';
 
 import styles from 'styles/directMessages/DirectMessages.module.scss';
 
 export default function DirectMessages() {
+  const { t } = useTranslation('common');
+  const [isEditable, setIsEditable] = useState<boolean>(false);
   const [{ chatList }, setChatList] = useState<ChatList>({ chatList: [] });
   const { get } = useCustomQuery();
   const { data, isLoading, isError } = get(
@@ -22,11 +27,26 @@ export default function DirectMessages() {
   if (isLoading) return <LoadingSpinner />;
   if (isError) return <ErrorRefresher />;
 
+  const handleEditClick = () => {
+    setIsEditable(!isEditable);
+  };
+
   return (
     <div className={styles.directMessagesContainer}>
-      {chatList.map((dmRoom: DMRoom, i: number) => {
-        return <DirectMessageBox key={i} dmRoom={dmRoom} />;
-      })}
+      <BasicButton
+        style='small'
+        color='purple'
+        handleButtonClick={handleEditClick}
+      >
+        {isEditable ? t('ok') : t('edit')}
+      </BasicButton>
+      <div className={styles.directMessagesWrap}>
+        {chatList.map((dmRoom: DMRoom, i: number) => {
+          return (
+            <DirectMessageBox key={i} dmRoom={dmRoom} isEditable={isEditable} />
+          );
+        })}
+      </div>
     </div>
   );
 }
