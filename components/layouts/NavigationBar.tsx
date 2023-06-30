11088894f66a5fd9e23a514cca2f7c0da01fae1f
triplecheeseburger <hangyulkim94@gmail.com>
2023-07-01 00:14:01 +0900

    [feat] navigation bar 비로그인 상태시 홈을 제외한 버튼들 비활성화 & 모달 팝 #310

--- components/layouts/NavigationBar.tsx
+++ components/layouts/NavigationBar.tsx
@@ -1,11 +1,18 @@
+import { useRecoilValue } from 'recoil';
+
 import Link from 'next/link';
 
+import React from 'react';
 import {
   IoMdChatboxes,
   IoMdHome,
   IoMdPeople,
   IoMdPerson,
 } from 'react-icons/io';
+
+import { loginState } from 'recoils/login';
+
+import useModalProvider from 'hooks/useModalProvider';
 
 import styles from 'styles/layouts/NavigationBar.module.scss';
 
@@ -15,6 +22,9 @@ type navigation = {
 };
 
 export default function NavigationBar() {
+  const login = useRecoilValue(loginState);
+  const { useNeedLoginModal } = useModalProvider();
+
   const navigations: navigation[] = [
     { value: <IoMdHome />, route: '/' },
     { value: <IoMdChatboxes />, route: '/channels' },
@@ -22,17 +32,40 @@ export default function NavigationBar() {
     { value: <IoMdPerson />, route: '/myPage' },
   ];
 
-  return (
-    <div className={styles.navigationBarContainer}>
-      {navigations.map(({ value, route }, i) => {
-        return (
-          <div key={i} className={styles.buttonBackground}>
-            <Link href={route} className={styles.button}>
-              {value}
-            </Link>
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (login)
+    return (
+      <div className={styles.navigationBarContainer}>
+        {navigations.map(({ value, route }, i) => {
+          return (
+            <div key={i} className={styles.buttonBackground}>
+              <Link href={route} className={styles.button}>
+                {value}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    );
+  else
+    return (
+      <div className={styles.navigationBarContainer}>
+        {navigations.map(({ value, route }, i) => {
+          if (route === '/')
+            return (
+              <div key={i} className={styles.buttonBackground}>
+                <Link href={route} className={styles.button}>
+                  {value}
+                </Link>
+              </div>
+            );
+          return (
+            <div key={i} className={styles.buttonBackground}>
+              <div className={styles.button} onClick={useNeedLoginModal}>
+                {value}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
 }
