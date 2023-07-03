7dbf62633bfc1ba002518d01b96ce3f5ba9a85e7
tamagoyakii <rlawlgus4063@naver.com>
2023-07-04 02:41:43 +0900

    [fix] 바뀐 초대 api 적용 #306

--- pages/api/games/invitation.ts
+++ pages/api/games/invitation.ts
@@ -5,8 +5,13 @@ type Error = {
 };
 
 export default (req: NextApiRequest, res: NextApiResponse<Error>) => {
-  if (req.method === 'DELETE')
+  const { nickname, mode } = req.body;
+
+  if (req.method === 'POST') {
+    res.status(200).json({
+      message: `invitation to ${nickname} for ${mode} game has been sent successfully`,
+    });
+  } else if (req.method === 'DELETE')
     res.status(200).json({ message: 'Canceled Invitation' });
-  else
-    res.status(405).json({ message: 'Method Not Allowed' });
+  else res.status(405).json({ message: 'Method Not Allowed' });
 };
--- pages/api/games/invitation/[idNickname].ts
+++ pages/api/games/invitation/[idNickname].ts
@@ -9,10 +9,7 @@ type GameId = {
 };
 
 export default (req: NextApiRequest, res: NextApiResponse<Error | GameId>) => {
-  const { mode } = req.body;
-  if (req.method === 'POST') {
-    res.status(200).json({ message: `Successfully Invited (${mode})` });
-  } else if (req.method === 'PATCH') {
+  if (req.method === 'PATCH') {
     res.status(200).json({ gameId: '1' });
   } else if (req.method === 'DELETE') {
     res.status(200).json({ message: 'Rejected Game Invitation' });
