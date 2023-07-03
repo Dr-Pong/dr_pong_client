eac47d2d473146d0782e3243e1cea5f2dcad8df7
tamagoyakii <rlawlgus4063@naver.com>
2023-07-04 02:36:16 +0900

    [fix] 채널과 게임 컴포넌트 분리한 것 적용 #306

--- components/global/InvitationRequest.tsx
+++ components/global/InvitationRequest.tsx
@@ -1,64 +1,72 @@
 import useTranslation from 'next-translate/useTranslation';
 
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 
-import { Participant } from 'types/chatTypes';
-import { Friend, Statuses } from 'types/friendTypes';
-import { InvitationType } from 'types/notificationTypes';
+import { Friend } from 'types/friendTypes';
 
-import useChatSocket from 'hooks/useChatSocket';
 import useFriendsQuery from 'hooks/useFriendsQuery';
 
+import ChannelInviteableFriendList from 'components/channels/ChannelInviteableFriendList';
+import GameInviteableFriendList from 'components/game/GameInviteableFriendList';
 import ErrorRefresher from 'components/global/ErrorRefresher';
-import FriendInvitationBox from 'components/global/FriendInvitationBox';
 import LoadingSpinner from 'components/global/LoadingSpinner';
 import SearchBar from 'components/global/SearchBar';
+import SocketManager from 'components/global/SocketManager';
 
 import styles from 'styles/global/InvitationRequest.module.scss';
 
-type InvitationProps = {
-  invitationType: InvitationType;
-  invitationArg: string;
-  participants?: Participant[];
+type InvitationRequestProps = {
+  type: string;
+  channelId?: string;
+  gameMode?: string;
+  participantNames?: string[];
 };
 
 export default function InvitationRequest({
-  invitationType,
-  invitationArg,
-  participants,
-}: InvitationProps) {
+  type,
+  channelId = '',
+  gameMode = '',
+  participantNames,
+}: InvitationRequestProps) {
   const { t } = useTranslation('common');
   const { allListGet } = useFriendsQuery();
   const [searchKey, setSearchKey] = useState<string>('');
   const [friends, setFriends] = useState<Friend[]>([]);
-  const [statuses, setStatuses] = useState<Statuses>({});
-  const { isLoading, isError, error } = allListGet(setFriends);
-  const [chatSocket] = useChatSocket('friends');
-
-  useEffect(() => {
-    const friendStatusListener = (newStatuses: Statuses) => {
-      setStatuses((prev) => {
-        return {
-          ...prev,
-          ...newStatuses,
-        };
-      });
-    };
-    chatSocket.on('friends', friendStatusListener);
-    return () => {
-      chatSocket.off('friends', friendStatusListener);
-    };
-  }, []);
+  const { isLoading, isError } = allListGet(setFriends);
 
   if (isLoading) return <LoadingSpinner />;
   if (isError) return <ErrorRefresher error={error} />;
 
-  const filteredFriends: Friend[] = friends.filter((friend) => {
-    const notDuplicate = !participants?.some(
-      (participant) => participant.nickname === friend.nickname
+  const filterChannelFriends = () => {
+    return friends.filter(
+      ({ nickname }) =>
+        nickname.includes(searchKey) && !participantNames?.includes(nickname)
     );
-    return friend.nickname.includes(searchKey) && notDuplicate;
-  });
+  };
+
+  const filterGameFriends = () => {
+    return friends.filter(({ nickname }) => nickname.includes(searchKey));
+  };
+
+  const friendList: { [key: string]: ReactNode } = {
+    channel: (
+      <ChannelInviteableFriendList
+        roomId={channelId}
+        friends={filterChannelFriends()}
+      />
+    ),
+    game: (
+      <>
+        <SocketManager namespace={'friends'} />
+        <GameInviteableFriendList
+          mode={gameMode}
+          friends={filterGameFriends()}
+        />
+      </>
+    ),
+  };
+
+  if (!friendList.hasOwnProperty(type)) return null;
 
   return (
     <div className={styles.invitationModal}>
@@ -68,19 +76,7 @@ export default function InvitationRequest({
         setSearchKey={setSearchKey}
         placeHolder={t('search by nickname')}
       />
-      <div className={styles.friendList}>
-        {filteredFriends.map((friend) => {
-          return (
-            <FriendInvitationBox
-              key={friend.nickname}
-              friend={friend}
-              status={statuses[friend.nickname]}
-              type={invitationType as InvitationType}
-              invitationArg={invitationArg}
-            />
-          );
-        })}
-      </div>
+      <div className={styles.friendList}>{friendList[type]}</div>
     </div>
   );
 }
