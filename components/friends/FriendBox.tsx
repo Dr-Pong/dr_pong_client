07156da88baaa938a2b329980e7db68329de70a9
tamagoyakii <rlawlgus4063@naver.com>
2023-07-04 02:21:11 +0900

    [delete] friend 페이지에서 사용되지 않는 부분 분리 #306

--- components/friends/FriendBox.tsx
+++ components/friends/FriendBox.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 
-import { Activity, Friend, FriendBoxType } from 'types/friendTypes';
+import { Activity, Friend, FriendTab } from 'types/friendTypes';
 
 import useModalProvider from 'hooks/useModalProvider';
 
 import styles from 'styles/friends/FriendBox.module.scss';
 
+import FriendButtons from './FriendButtons';
+
 type FriendBoxProps = {
-  children: React.ReactNode;
-  type: FriendBoxType;
+  tab: FriendTab;
   friend: Friend;
   status?: Activity;
 };
 
-export default function FriendBox({
-  type,
-  friend,
-  status,
-  children,
-}: FriendBoxProps) {
+export default function FriendBox({ tab, friend, status }: FriendBoxProps) {
   const { nickname, imgUrl } = friend;
   const { useProfileModal } = useModalProvider();
 
@@ -28,7 +24,7 @@ export default function FriendBox({
   };
 
   return (
-    <div className={`${styles.friendBoxContainer} ${styles[type]}`}>
+    <div className={styles.friendBoxContainer}>
       <div className={styles.imageStatusWrap}>
         <img className={styles.img} src={imgUrl} alt={`photo of ${nickname}`} />
         {status && (
@@ -38,7 +34,7 @@ export default function FriendBox({
       <div className={styles.nickname} onClick={handleNicknameClick}>
         {nickname}
       </div>
-      {children}
+      <FriendButtons tab={tab} nickname={friend.nickname} />
     </div>
   );
 }
