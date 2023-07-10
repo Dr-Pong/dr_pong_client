ec9ddf3bdef171a7b9901e47ddaeffdc80c3843b
triplecheeseburger <hangyulkim94@gmail.com>
2023-07-10 19:32:29 +0900

    [feat] 사운드를 관리하기 위한 설정 메뉴 #338

---
+++ components/settings/Bgm.tsx
@@ -0,0 +1,41 @@
+import { useRecoilState } from 'recoil';
+
+import React from 'react';
+import { TbMusic, TbMusicOff } from 'react-icons/tb';
+
+import { bgmState } from 'recoils/sound';
+
+import { useBgm } from 'hooks/useBgm';
+
+import BasicButton from 'components/global/buttons/BasicButton';
+
+import styles from 'styles/settings/TfaField.module.scss';
+
+export default function Bgm() {
+  const [bgmOn, setBgmOn] = useRecoilState(bgmState);
+  const { audios, loaded } = useBgm({
+    bgm: '/sound/bgm.mp3',
+  });
+  const enableBgm = () => {
+    audios.get('bgm')();
+    setBgmOn(true);
+  };
+
+  const disableBgm = () => {
+    audios.get('bgm_stop')();
+    setBgmOn(false);
+  };
+
+  if (!loaded) return null;
+  return (
+    <div className={styles.tfaFieldContainer}>
+      <BasicButton
+        style='basic'
+        color='purple'
+        handleButtonClick={bgmOn ? disableBgm : enableBgm}
+      >
+        {bgmOn ? <TbMusicOff /> : <TbMusic />}
+      </BasicButton>
+    </div>
+  );
+}
---
+++ components/settings/SoundEffect.tsx
@@ -0,0 +1,41 @@
+import { useRecoilState } from 'recoil';
+
+import React from 'react';
+import { HiSpeakerWave, HiSpeakerXMark } from 'react-icons/hi2';
+
+import { soundEffectState } from 'recoils/sound';
+
+import { useSoundEffect } from 'hooks/useSoundEffect';
+
+import BasicButton from 'components/global/buttons/BasicButton';
+
+import styles from 'styles/settings/TfaField.module.scss';
+
+export default function SoundEffect() {
+  const [soundEffectOn, setSoundEffectOn] = useRecoilState(soundEffectState);
+  const { audios, loaded } = useSoundEffect({
+    cork: 'sound/cork.mp3',
+  });
+  const enableBgm = () => {
+    setSoundEffectOn(true);
+    if (loaded) audios.get('cork')(true);
+  };
+
+  const disableBgm = () => {
+    setSoundEffectOn(false);
+  };
+
+  if (!loaded) return null;
+
+  return (
+    <div className={styles.tfaFieldContainer}>
+      <BasicButton
+        style='basic'
+        color='purple'
+        handleButtonClick={soundEffectOn ? disableBgm : enableBgm}
+      >
+        {soundEffectOn ? <HiSpeakerXMark /> : <HiSpeakerWave />}
+      </BasicButton>
+    </div>
+  );
+}
