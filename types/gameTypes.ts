5c2fa26111fcf4394df2d337e8e4e2e76004caee
triplecheeseburger <hangyulkim94@gmail.com>
2023-07-12 15:56:20 +0900

    [feat] 타입 추가 #345

--- types/gameTypes.ts
+++ types/gameTypes.ts
@@ -15,29 +15,35 @@ export interface Ball {
 }
 
 export interface posData {
-  ballPos: { x: number, y: number };
-  playerXPos: { me: number, opponent: number };
-};
+  ballPos: { x: number; y: number };
+  playerXPos: { me: number; opponent: number };
+}
 
 export interface roundData {
   round: number;
   me: number;
   opponent: number;
   server: boolean;
-};
+}
 
 export interface initData {
-  me: { x: number, y: number, width: number, height: number };
-  opponent: { x: number, y: number, width: number, height: number };
-  ball: { x: number, y: number, size: number };
+  me: { x: number; y: number; width: number; height: number };
+  opponent: { x: number; y: number; width: number; height: number };
+  ball: { x: number; y: number; size: number };
   server: boolean;
   round: number;
-};
+}
 
 export interface countdownData {
   time: number;
-};
+}
 
 export interface gameResult {
   result: 'win' | 'lose' | 'tie';
-};
+}
+
+export interface ExpResult {
+  beforeExp: number;
+  expChange: number;
+  levelExp: number;
+}
