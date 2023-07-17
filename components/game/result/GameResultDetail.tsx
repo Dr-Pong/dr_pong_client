c51b284168d77f53a41345ddd9880b8ada0556d3
triplecheeseburger <hangyulkim94@gmail.com>
2023-07-17 13:33:53 +0900

    [fix] options 추출 ! #344

--- components/game/result/GameResultDetail.tsx
+++ components/game/result/GameResultDetail.tsx
@@ -1,4 +1,4 @@
-import { Fireworks } from 'fireworks-js';
+import { Fireworks, FireworksTypes } from 'fireworks-js';
 import useTranslation from 'next-translate/useTranslation';
 import { useRecoilValue } from 'recoil';
 
@@ -32,55 +32,7 @@ export default function GameResultDetail({
   useEffect(() => {
     if (showFireworks) {
       const container = document.getElementById('fireworks');
-      const fireworks = new Fireworks(container, {
-        autoresize: true,
-        opacity: 0.5,
-        acceleration: 1.05,
-        friction: 0.97,
-        gravity: 1.5,
-        particles: 50,
-        traceLength: 3,
-        traceSpeed: 10,
-        explosion: 5,
-        intensity: 30,
-        flickering: 50,
-        lineStyle: 'round',
-        hue: {
-          min: 0,
-          max: 360,
-        },
-        delay: {
-          min: 30,
-          max: 60,
-        },
-        rocketsPoint: {
-          min: 50,
-          max: 50,
-        },
-        lineWidth: {
-          explosion: {
-            min: 1,
-            max: 3,
-          },
-          trace: {
-            min: 1,
-            max: 2,
-          },
-        },
-        brightness: {
-          min: 50,
-          max: 80,
-        },
-        decay: {
-          min: 0.015,
-          max: 0.03,
-        },
-        mouse: {
-          click: false,
-          move: false,
-          max: 1,
-        },
-      });
+      const fireworks = new Fireworks(container, options);
       fireworks.start();
     }
   }, [showFireworks]);
@@ -102,3 +54,53 @@ export default function GameResultDetail({
     </div>
   );
 }
+
+const options = {
+  autoresize: true,
+  opacity: 0.5,
+  acceleration: 1.05,
+  friction: 0.97,
+  gravity: 1.5,
+  particles: 50,
+  traceLength: 3,
+  traceSpeed: 10,
+  explosion: 5,
+  intensity: 30,
+  flickering: 50,
+  lineStyle: 'round' as FireworksTypes.LineStyle,
+  hue: {
+    min: 0,
+    max: 360,
+  },
+  delay: {
+    min: 30,
+    max: 60,
+  },
+  rocketsPoint: {
+    min: 50,
+    max: 50,
+  },
+  lineWidth: {
+    explosion: {
+      min: 1,
+      max: 3,
+    },
+    trace: {
+      min: 1,
+      max: 2,
+    },
+  },
+  brightness: {
+    min: 50,
+    max: 80,
+  },
+  decay: {
+    min: 0.015,
+    max: 0.03,
+  },
+  mouse: {
+    click: false,
+    move: false,
+    max: 1,
+  },
+};
