28abec90a9c8bb51f91eb2118a4f3149125845ce
keonwoo98 <keonwoo98@naver.com>
2023-07-18 20:36:23 +0900

    [feat] global socket에 다중 로그인 리스너 등록 #359

--- components/layouts/AppLayout.tsx
+++ components/layouts/AppLayout.tsx
@@ -1,5 +1,11 @@
 import { LayoutProps } from 'pages/_app';
 
+import React, { useEffect } from 'react';
+
+import useChatSocket from 'hooks/useChatSocket';
+import useUpperModalProvider from 'hooks/useUpperModalProvider';
+import useAuthHandler from 'hooks/useAuthHandler';
+
 import Alert from 'components/alerts/Alert';
 import NavigationBar from 'components/layouts/NavigationBar';
 import Header from 'components/layouts/header/Header';
@@ -9,6 +15,26 @@ import UpperModal from 'components/modals/UpperModal';
 import styles from 'styles/layouts/Layout.module.scss';
 
 export default function AppLayout({ children }: LayoutProps) {
+  const [socket] = useChatSocket('global');
+  const { onLogout } = useAuthHandler();
+  const { useIsInGameModal, multiConnectWarningMoal } = useUpperModalProvider();
+
+  useEffect(() => {
+    const isInGameListener = (data: { roomId: string }) => {
+      if (!data.roomId) return;
+      useIsInGameModal(data.roomId);
+    };
+    const multiConnectListener = () => {
+      multiConnectWarningMoal(onLogout);
+    };
+    socket.on('isInGame', isInGameListener);
+    socket.on('multiConnect', multiConnectListener);
+    return () => {
+      socket.off('isInGame', isInGameListener);
+      socket.off('multiConnect', multiConnectListener);
+    };
+  }, []);
+
   return (
     <div id='layoutRoot'>
       <Modal />
--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,14 +1,36 @@
 import { LayoutProps } from 'pages/_app';
 
+import React, { useEffect } from 'react';
+
+import useChatSocket from 'hooks/useChatSocket';
+import useUpperModalProvider from 'hooks/useUpperModalProvider';
+import useAuthHandler from 'hooks/useAuthHandler';
+
 import Alert from 'components/alerts/Alert';
 import Modal from 'components/modals/Modal';
+import UpperModal from 'components/modals/UpperModal';
 
 import styles from 'styles/layouts/Layout.module.scss';
 
 export default function Layout({ children }: LayoutProps) {
+  const [socket] = useChatSocket('global');
+  const { onLogout } = useAuthHandler();
+  const { multiConnectWarningMoal } = useUpperModalProvider();
+
+  useEffect(() => {
+    const multiConnectListener = () => {
+      multiConnectWarningMoal(onLogout);
+    };
+    socket.on('multiConnect', multiConnectListener);
+    return () => {
+      socket.off('multiConnect', multiConnectListener);
+    }
+  }, []);
+
   return (
     <div id='layoutRoot'>
       <Modal />
+      <UpperModal />
       <Alert />
       <div className={styles.layoutContainer}>
         <div className={styles.pageContainer}>{children}</div>
