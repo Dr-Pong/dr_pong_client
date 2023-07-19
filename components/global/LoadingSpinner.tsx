20afb251ee528dc7294cf40fb91063a136e81a4d
triplecheeseburger <hangyulkim94@gmail.com>
2023-07-19 15:15:01 +0900

    [feat] 보여줄게 완전히 달라진 나 로딩 스피너 타이머를 쓸 수 있다 ! #380

--- components/global/LoadingSpinner.tsx
+++ components/global/LoadingSpinner.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from 'styles/global/LoadingSpinner.module.scss';
 
 export default function LoadingSpinner() {
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className={styles.loadingContainer}>
       <div className={styles.loadingSpinner} />
+      <div className={styles.time}>{seconds}</div>
     </div>
   );
-};
+}
