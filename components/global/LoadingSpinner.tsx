f96d09e40d85cf0f1a2abe9a6e4cb11036327e5e
triplecheeseburger <hangyulkim94@gmail.com>
2023-07-19 15:39:52 +0900

    [fix] 누락된 부분 올립니다.. #380

--- components/global/LoadingSpinner.tsx
+++ components/global/LoadingSpinner.tsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 import styles from 'styles/global/LoadingSpinner.module.scss';
 
-export default function LoadingSpinner() {
+export default function LoadingSpinner({ useTimer }: { useTimer?: boolean }) {
   const [seconds, setSeconds] = useState(0);
+
   useEffect(() => {
+    if (!useTimer) return;
     const intervalId = setInterval(() => {
       setSeconds((prevSeconds) => prevSeconds + 1);
     }, 1000);
@@ -14,7 +16,7 @@ export default function LoadingSpinner() {
   return (
     <div className={styles.loadingContainer}>
       <div className={styles.loadingSpinner} />
-      <div className={styles.time}>{seconds}</div>
+      {useTimer && <div className={styles.time}>{seconds}</div>}
     </div>
   );
 }
