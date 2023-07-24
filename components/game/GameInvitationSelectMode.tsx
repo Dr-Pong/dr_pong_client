4f1ce94af777b2b2354b1ac98119d753a7575871
keonwoo98 <keonwoo98@naver.com>
2023-07-24 16:59:21 +0900

    [feat] 프로필 모달에서 게임 초대 시 모드 선택 모달 #387

---
+++ components/game/GameInvitationSelectMode.tsx
@@ -0,0 +1,54 @@
+import useTranslation from 'next-translate/useTranslation';
+import React, { ChangeEvent, useState } from 'react';
+
+import GameInvitationButton from 'components/game/GameInvitationButton';
+
+import { ButtonDesign } from 'types/buttonTypes';
+
+import styles from 'styles/game/GameInvitationSelectMode.module.scss';
+
+const button: ButtonDesign = {
+  style: 'long',
+  color: 'pink',
+};
+
+export default function GameInvitableSelectMode(
+  { target }: { target: string }) {
+  const { t } = useTranslation('game');
+  const modeList = ['classic', 'randomBounce'];
+  const [gameMode, setGameMode] = useState<string>('classic');
+
+  const handleModeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setGameMode(value);
+  };
+
+  return (
+    <div className={styles.contents}>
+      <div className={styles.modeList}>
+        {modeList.map((mode, i) => {
+          return (
+            <label key={i} className={styles.radio} htmlFor={mode}>
+              <input
+                type='radio'
+                id={mode}
+                name='mode'
+                value={mode}
+                checked={gameMode === mode}
+                onChange={handleModeChange}
+              />
+              {t(mode)}
+            </label>
+          );
+        })}
+      </div>
+      <GameInvitationButton
+        nickname={target}
+        mode={gameMode}
+        buttonDesign={button}
+      >
+        {t('invite')}
+      </GameInvitationButton>
+    </div>
+  );
+}
