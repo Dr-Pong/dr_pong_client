81db40335c7a98792833c18a3f6408930ae36ed4
keonwoo98 <keonwoo98@naver.com>
2023-07-28 16:00:34 +0900

    [feat] Channel 입장시 본인의 mute 상태 확인 후 적용 #419

--- components/chats/Chattings.tsx
+++ components/chats/Chattings.tsx
@@ -25,12 +25,14 @@ type ChattingsProps = {
   userImageMap: UserImageMap;
   roomType: RoomType;
   roomId: string;
+  isMuted: boolean;
 };
 
 export default function Chattings({
   userImageMap,
   roomType,
   roomId,
+  isMuted,
 }: ChattingsProps) {
   const router = useRouter();
   const queryClient = useQueryClient();
@@ -41,7 +43,7 @@ export default function Chattings({
   const [isTopRefVisible, setIsTopRefVisible] = useState(true);
   const [isBottomRefVisible, setIsBottomRefVisible] = useState(false);
   const [newestChat, setNewestChat] = useState<Chat | null>(null);
-  const [inputDisabled, setInputDisables] = useState(false);
+  const [inputDisabled, setInputDisables] = useState(isMuted);
   const { chatsGet } = useChatQuery(roomType, roomId);
   const { mutate } = useChatQuery(roomType, roomId).postChatMutation();
   const topRef = useRef<HTMLDivElement>(null);
--- components/chats/ChattingsFrame.tsx
+++ components/chats/ChattingsFrame.tsx
@@ -82,6 +82,7 @@ export default function ChattingsFrame({
         userImageMap={userImageMap}
         roomType={roomType as RoomType}
         roomId={roomId as string}
+        isMuted={me.isMuted}
       />
     </>
   );
