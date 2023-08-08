447532380553a1066855344ef5b8c314c2b89b12
tamagoyakii <rlawlgus4063@naver.com>
2023-08-08 15:51:26 +0900

    [fix] ref 수정중 #449

--- components/chats/Chattings.tsx
+++ components/chats/Chattings.tsx
@@ -40,12 +40,13 @@ export default function Chattings({
   const setAlert = useSetRecoilState(alertState);
   const [chats, setChats] = useState<Chat[]>([]);
   const [message, setMessage] = useState<string>('');
-  const [isTopRefVisible, setIsTopRefVisible] = useState(false);
-  const [isBottomRefVisible, setIsBottomRefVisible] = useState(true);
+  // const [isTopRefVisible, setIsTopRefVisible] = useState(false);
+  // const [isBottomRefVisible, setIsBottomRefVisible] = useState(true);
   const [showPreview, setShowPreview] = useState<boolean>(false);
   const [inputDisabled, setInputDisabled] = useState(isMuted);
   const [failedChatCount, setFailedChatCount] = useState<number>(0);
-  const topRef = useRef<HTMLDivElement>(null);
+  const [newestChat, setNewestChat] = useState<Chat | null>(null);
+  // const topRef = useRef<HTMLDivElement>(null);
   const bottomRef = useRef<HTMLDivElement>(null);
   const [socket] = useChatSocket(roomType);
   const { queryClient, mutationPost } = useCustomQuery();
@@ -69,10 +70,15 @@ export default function Chattings({
       },
     }
   );
+  const messageRef = useRef<HTMLDivElement>(null);
 
   const newMessageListener = (data: Chat) => {
+    // const ref = messageRef.current!;
+
     setChats((prev) => [data, ...prev]);
-    if (!isBottomRefVisible && data.type === 'others') setShowPreview(true);
+    if (data.type === 'others') setNewestChat(data);
+    // if (!isBottomRefVisible && data.type === 'others') setShowPreview(true);
+    // ref.scrollTop = ref.scrollHeight;
   };
 
   const newSystemMessageListener = (data: Chat) => {
@@ -96,6 +102,8 @@ export default function Chattings({
   };
 
   useEffect(() => {
+    if (messageRef.current)
+      messageRef.current!.addEventListener('scroll', handleScroll);
     socket.on('message', newMessageListener);
     socket.on('system', newSystemMessageListener);
     socket.on('out', kickBanListener);
@@ -105,6 +113,7 @@ export default function Chattings({
       socket.emit('dear', { nickname: roomId });
     }
     return () => {
+      messageRef.current!.removeEventListener('scroll', handleScroll);
       socket.off('message', newMessageListener);
       socket.off('system', newSystemMessageListener);
       socket.off('out', kickBanListener);
@@ -113,49 +122,58 @@ export default function Chattings({
     };
   }, []);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.target === bottomRef.current) {
-            setIsBottomRefVisible(entry.isIntersecting);
-            setShowPreview(false);
-          } else if (entry.target === topRef.current) {
-            setIsTopRefVisible(entry.isIntersecting);
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.target === bottomRef.current) {
+  //           setIsBottomRefVisible(entry.isIntersecting);
+  //           setShowPreview(false);
+  //         } else if (entry.target === topRef.current) {
+  //           setIsTopRefVisible(entry.isIntersecting);
+  //         }
+  //       });
+  //     },
+  //     { threshold: 0.5 }
+  //   );
 
-    const timeout = setTimeout(() => {
-      if (topRef.current) observer.observe(topRef.current);
-      if (bottomRef.current) observer.observe(bottomRef.current);
-    }, 300);
+  //   const timeout = setTimeout(() => {
+  //     if (topRef.current) observer.observe(topRef.current);
+  //     if (bottomRef.current) observer.observe(bottomRef.current);
+  //   }, 300);
 
-    return () => {
-      clearTimeout(timeout);
-      if (topRef.current) observer.unobserve(topRef.current);
-      if (bottomRef.current) observer.unobserve(bottomRef.current);
-      queryClient.removeQueries(['chats', roomId]);
-    };
-  }, []);
+  //   return () => {
+  //     clearTimeout(timeout);
+  //     if (topRef.current) observer.unobserve(topRef.current);
+  //     if (bottomRef.current) observer.unobserve(bottomRef.current);
+  //     queryClient.removeQueries(['chats', roomId]);
+  //   };
+  // }, []);
 
   const handleChatJoin = (rawChats: Chat[]): void => {
     setChats((prev) => prev.concat(rawChats));
   };
 
+  // const handlePreviewClick = useCallback(() => {
+  //   bottomRef.current?.scrollIntoView({ behavior: 'auto' });
+  //   setShowPreview(false);
+  // }, [bottomRef.current]);
+
   const handlePreviewClick = useCallback(() => {
-    bottomRef.current?.scrollIntoView({ behavior: 'auto' });
+    const ref = messageRef.current!;
+
     setShowPreview(false);
-  }, [bottomRef.current]);
+    ref.scrollTop = ref.scrollHeight;
+  }, [showPreview]);
 
   const handleChatPost = useCallback(
     (message: string) => {
       if (!isMessageValid(message)) return;
+      const ref = messageRef.current!;
+
       sendChatMutaion.mutate({ message });
       setMessage('');
-      bottomRef.current?.scrollIntoView({ behavior: 'auto' });
+      ref.scrollTop = ref.scrollHeight;
     },
     [message]
   );
@@ -169,19 +187,33 @@ export default function Chattings({
     error,
   } = chatsGet(handleChatJoin, 20);
 
-  useEffect(() => {
-    if (hasNextPage && !isFetchingNextPage && isTopRefVisible) fetchNextPage();
-  }, [hasNextPage, fetchNextPage, isTopRefVisible]);
+  let prevScrollHeight = 0;
+
+  const handleScroll = () => {
+    const ref = messageRef.current!;
+    const scrollTop = ref.scrollTop;
+
+    if (hasNextPage && !isFetchingNextPage && scrollTop === 0) {
+      fetchNextPage();
+      prevScrollHeight = ref.scrollHeight;
+    }
+    if (prevScrollHeight - scrollTop > ref.clientHeight + 1000)
+      setShowPreview(true);
+  };
+
+  // useEffect(() => {
+  //   if (hasNextPage && !isFetchingNextPage && isTopRefVisible) fetchNextPage();
+  // }, [hasNextPage, fetchNextPage, isTopRefVisible]);
 
   if (isLoading) return <LoadingSpinner />;
   if (isError) return <ErrorRefresher error={error} />;
 
   return (
     <div className={styles.chattingsContainer}>
-      <div className={styles.chatRefWrap}>
-        <div ref={bottomRef} className={styles.ref}>
+      <div className={styles.chatRefWrap} ref={messageRef}>
+        {/* <div ref={bottomRef} className={styles.ref}>
           ㅤ
-        </div>
+        </div> */}
         {chats.map((chat) => {
           const { id, message, nickname } = chat;
           return (
@@ -201,11 +233,11 @@ export default function Chattings({
             </div>
           );
         })}
-        <div ref={topRef} className={styles.ref}>
+        {/* <div ref={topRef} className={styles.ref}>
           ㅤ
-        </div>
+        </div> */}
       </div>
-      {showPreview && (
+      {showPreview && newestChat && (
         <div className={styles.preview} onClick={handlePreviewClick}>
           <div>{chats[0].nickname}</div>
           <div>{messageCutter(chats[0].message)}</div>
