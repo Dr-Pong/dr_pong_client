e5b501ebb00d54cbec739b48aa732ee0c223c2b8
triplecheeseburger <hangyulkim94@gmail.com>
2023-08-10 23:42:07 +0900

    [feat] 광고와 통계용 헤더 스크립트 생성 #458

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,30 @@ import React from 'react';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <script
+          async
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5861134754944224'
+          crossOrigin='anonymous'
+        ></script>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        ></script>
+        <script
+          async
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
