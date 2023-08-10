7a2c7fd8f0bae95197e45f3e034296190c5930ad
triplecheeseburger <hangyulkim94@gmail.com>
2023-08-10 23:46:26 +0900

    [feat] 광고를 담기 위한 광고 레이아웃 만들기 #458

---
+++ components/layouts/AdsLayout.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router';
+
+import React, { useEffect, useState } from 'react';
+
+import { LayoutProps } from 'pages/_app';
+
+import GoogleAd from 'components/global/GoogleAd';
+
+import styles from 'styles/layouts/AdsLayout.module.scss';
+
+export default function AdsLayout({ children }: LayoutProps) {
+  const [direction, setDirection] = useState<'row' | 'column' | null>(null);
+  const router = useRouter();
+  const handleResize = () => {
+    setDirection(
+      window.innerHeight / window.innerWidth < 1.3 ? 'row' : 'column'
+    );
+  };
+
+  useEffect(() => {
+    setDirection(
+      window.innerHeight / window.innerWidth < 1.3 ? 'row' : 'column'
+    );
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (!direction || router.query.roomId) return <>{children}</>;
+  if (direction === 'row') {
+    return (
+      <div className={styles.adsLayoutRow}>
+        <GoogleAd
+          client='ca-pub-5861134754944224'
+          slot='4781852804'
+          format='auto'
+          responsive='true'
+        />
+        {children}
+        <GoogleAd
+          client='ca-pub-5861134754944224'
+          slot='4781852804'
+          format='auto'
+          responsive='true'
+        />
+      </div>
+    );
+  }
+  return (
+    <div className={styles.adsLayoutColumn}>
+      {children}
+      <GoogleAd
+        client='ca-pub-5861134754944224'
+        slot='9772393400'
+        format='auto'
+        responsive='true'
+      />
+    </div>
+  );
+}
