69d2b8c30db7546be043b928b851d48ba110407e
tamagoyakii <rlawlgus4063@naver.com>
2023-08-16 20:27:43 +0900

    [style/refactor] 스타일 정리 및 버튼 컴포넌트 분리 #462

---
+++ components/game/GameButtons.tsx
@@ -0,0 +1,30 @@
+import { useTranslation } from 'react-i18next';
+
+import styles from 'styles/game/GameButtons.module.scss';
+
+type GameButtonsProps = {
+  buttons: {
+    value: string;
+    color: string;
+    handleButtonClick: () => void;
+  }[];
+};
+
+export function GameButtons({ buttons }: GameButtonsProps) {
+  const { t } = useTranslation('game');
+
+  return (
+    <div className={styles.buttonList}>
+      {buttons.map(({ value, color, handleButtonClick }) => {
+        return (
+          <button
+            className={`${styles.button} ${styles[color]}`}
+            onClick={handleButtonClick}
+          >
+            {t(value)}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
