3529c047b96d9777d6b9de91bbf1b88e774c908b
keonwoo98 <keonwoo98@naver.com>
2023-08-17 16:42:14 +0900

    [feat] 이미 큐가 돌아가고 있거나 이미 게임중일 때 큐 입장하려하면 그에 맞는 에러 메시지 띄워주기 #472

--- components/game/GameLobby.tsx
+++ components/game/GameLobby.tsx
@@ -1,3 +1,5 @@
+import { AxiosError } from 'axios';
+
 import useTranslation from 'next-translate/useTranslation';
 import { useSetRecoilState } from 'recoil';
 
@@ -47,10 +49,13 @@ export default function GameLobby({ handleGoBackClick }: GameLobbyProps) {
       useMatchWaitingUpperModal(exitQueue.mutate, true);
       socket.once('matched', matchedListener);
     },
-    onError: () => {
-      setAlert({ type: 'failure' });
+    onError: (error: AxiosError<Error>) => {
+      setAlert({
+        type: 'failure',
+        message: t(`${error.response?.data.message}`)
+      });
     },
-  });
+  } as object);
 
   const modeList = ['classic', 'randomBounce'];
 
