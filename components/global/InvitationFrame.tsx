b8b32bf9a163556058a1c2c98d51c1c755761280
triplecheeseburger <hangyulkim94@gmail.com>
2023-08-18 11:11:43 +0900

    [fix] 채널 초대 모달에선 Participants 또 받아오게 요청 추가하기 & 게임 초대와 분리하기 #478

--- components/global/InvitationFrame.tsx
+++ components/global/InvitationFrame.tsx
@@ -1,9 +1,11 @@
 import useTranslation from 'next-translate/useTranslation';
 
-import React, { ReactNode, useState } from 'react';
+import React, { useState } from 'react';
 
+import { Participant } from 'types/chatTypes';
 import { Friend } from 'types/friendTypes';
 
+import useChatQuery from 'hooks/useChatQuery';
 import useFriendsQuery from 'hooks/useFriendsQuery';
 
 import ChannelInvitableFriendList from 'components/channels/ChannelInvitableFriendList';
@@ -16,17 +18,15 @@ import SocketManager from 'components/global/SocketManager';
 import styles from 'styles/global/InvitationFrame.module.scss';
 
 type InvitationFrameProps = {
-  type: string;
+  type: 'game' | 'channel';
   channelId?: string;
   gameMode?: string;
-  participantNames?: string[];
 };
 
 export default function InvitationFrame({
   type,
   channelId = '',
   gameMode = '',
-  participantNames,
 }: InvitationFrameProps) {
   const { t } = useTranslation('common');
   const { allListGet } = useFriendsQuery();
@@ -37,37 +37,6 @@ export default function InvitationFrame({
   if (isLoading) return <LoadingSpinner />;
   if (isError) return <ErrorRefresher error={error} />;
 
-  const filterChannelFriends = () => {
-    return friends.filter(
-      ({ nickname }) =>
-        nickname.includes(searchKey) && !participantNames?.includes(nickname)
-    );
-  };
-
-  const filterGameFriends = () => {
-    return friends.filter(({ nickname }) => nickname.includes(searchKey));
-  };
-
-  const friendList: { [key: string]: ReactNode } = {
-    channel: (
-      <ChannelInvitableFriendList
-        roomId={channelId}
-        friends={filterChannelFriends()}
-      />
-    ),
-    game: (
-      <>
-        <SocketManager namespace={'friends'} />
-        <GameInvitableFriendList
-          mode={gameMode}
-          friends={filterGameFriends()}
-        />
-      </>
-    ),
-  };
-
-  if (!Object.prototype.hasOwnProperty.call(friendList, type)) return null;
-
   return (
     <div className={styles.InvitationFrameContainer}>
       <SearchBar
@@ -76,7 +45,74 @@ export default function InvitationFrame({
         setSearchKey={setSearchKey}
         placeHolder={t('search by nickname')}
       />
-      <div className={styles.friendList}>{friendList[type]}</div>
+      {type === 'channel' ? (
+        <ChannelInvitationFrame
+          channelId={channelId}
+          searchKey={searchKey}
+          friends={friends}
+        />
+      ) : (
+        <GameInvitationFrame
+          gameMode={gameMode}
+          searchKey={searchKey}
+          friends={friends}
+        />
+      )}
+    </div>
+  );
+}
+
+function ChannelInvitationFrame({
+  channelId,
+  searchKey,
+  friends,
+}: {
+  channelId: string;
+  searchKey: string;
+  friends: Friend[];
+}) {
+  const { participantsGet } = useChatQuery('channel', channelId);
+
+  const { data, isLoading, isError, error } = participantsGet()();
+  if (isLoading) return <LoadingSpinner />;
+  if (isError) return <ErrorRefresher error={error} />;
+
+  const participants = data.participants.map((p: Participant) => p.nickname);
+
+  const filterChannelFriends = () => {
+    return friends.filter(
+      ({ nickname }) =>
+        nickname.includes(searchKey) && !participants?.includes(nickname)
+    );
+  };
+
+  return (
+    <div className={styles.friendList}>
+      <ChannelInvitableFriendList
+        roomId={channelId}
+        friends={filterChannelFriends()}
+      />
+    </div>
+  );
+}
+
+function GameInvitationFrame({
+  gameMode,
+  searchKey,
+  friends,
+}: {
+  gameMode: string;
+  searchKey: string;
+  friends: Friend[];
+}) {
+  const filterGameFriends = () => {
+    return friends.filter(({ nickname }) => nickname.includes(searchKey));
+  };
+
+  return (
+    <div className={styles.friendList}>
+      <SocketManager namespace={'friends'} />
+      <GameInvitableFriendList mode={gameMode} friends={filterGameFriends()} />
     </div>
   );
 }
