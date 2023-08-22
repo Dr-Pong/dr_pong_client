c1593f5f0fd3b1043787f4eaf130c8d9f44d35dd
keonwoo98 <keonwoo98@naver.com>
2023-08-22 15:20:44 +0900

    [fix] children을 조건부 렌더링에 영향을 받지 않게 조건 밖으로 뺴주기 #488

--- components/layouts/AdsLayout.tsx
+++ components/layouts/AdsLayout.tsx
@@ -8,6 +8,10 @@ import GoogleAd from 'components/global/GoogleAd';
 
 import styles from 'styles/layouts/AdsLayout.module.scss';
 
+const isTouchScreen =
+  typeof window !== 'undefined' &&
+  window.matchMedia('(hover: none) and (pointer: coarse)').matches;
+
 export default function AdsLayout({ children }: LayoutProps) {
   const [direction, setDirection] = useState<'row' | 'column'>(window.innerHeight / window.innerWidth < 1.3 ? 'row' : 'column');
   const router = useRouter();
@@ -25,34 +29,40 @@ export default function AdsLayout({ children }: LayoutProps) {
     };
   }, []);
 
-  if (direction === 'row' || router.query.roomId) {
-    return (
-      <div className={styles.adsLayoutRow}>
-        <GoogleAd
-          client='ca-pub-5861134754944224'
-          slot='4781852804'
-          format='vertical'
-          responsive='true'
-        />
-        {children}
-        <GoogleAd
-          client='ca-pub-5861134754944224'
-          slot='4781852804'
-          format='vertical'
-          responsive='true'
-        />
-      </div>
-    );
-  }
+  if (isTouchScreen && router.query.roomId) return <>{children}</>;
+
   return (
-    <div className={styles.adsLayoutColumn}>
+    <div
+      className={
+        direction === 'row' || router.query.roomId
+          ? styles.adsLayoutRow
+          : styles.adsLayoutColumn
+      }
+    >
+      {(direction === 'row' || router.query.roomId) && (
+        <GoogleAd
+          client='ca-pub-5861134754944224'
+          slot='4781852804'
+          format='vertical'
+          responsive='true'
+        />
+      )}
       {children}
-      <GoogleAd
-        client='ca-pub-5861134754944224'
-        slot='9772393400'
-        format='horizontal'
-        responsive='true'
-      />
+      {(direction === 'row' && !router.query.roomId) ? (
+        <GoogleAd
+          client='ca-pub-5861134754944224'
+          slot='4781852804'
+          format='vertical'
+          responsive='true'
+        />
+      ) : (
+        <GoogleAd
+          client='ca-pub-5861134754944224'
+          slot='9772393400'
+          format='horizontal'
+          responsive='true'
+        />
+      )}
     </div>
   );
 }
