888c8bcbca2d172bb0c9f8f9bebbce25ce63d905
tamagoyakii <rlawlgus4063@naver.com>
2023-08-31 20:37:41 +0900

    [delete] Tier 삭제 및 리팩토링 및 리디자인 #535

--- components/myPage/profile/StatCard.tsx
+++ components/myPage/profile/StatCard.tsx
@@ -8,31 +8,23 @@ import { IoIosArrowForward } from 'react-icons/io';
 
 import { sideBarState } from 'recoils/sideBar';
 
-import { ProfileStyle, StatRankResponse } from 'types/userTypes';
+import { ProfileStyle } from 'types/userTypes';
 
 import useModalProvider from 'hooks/useModalProvider';
 import useMyPageQuery from 'hooks/useMyPageQuery';
 
-import ErrorRefresher from 'components/global/ErrorRefresher';
 import LoadingSpinner from 'components/global/LoadingSpinner';
-import BestRecord from 'components/myPage/profile/BestRecord';
 import RankTag from 'components/myPage/profile/RankTag';
-import TierLp from 'components/myPage/profile/TierLp';
 import WinRateStat from 'components/myPage/profile/WinRateStat';
 
 import styles from 'styles/myPage/StatCard.module.scss';
-
-type StatBox = {
-  top: React.ReactElement;
-  bottom: React.ReactElement;
-};
 
 type StatCardProps = { nickname: string; style: ProfileStyle };
 
 export default function StatCard({ nickname, style }: StatCardProps) {
   const { statGet } = useMyPageQuery(nickname);
   const { t } = useTranslation('myPage');
-  const { data, isLoading, isError } = statGet();
+  const { data, isLoading } = statGet();
   const setSidebar = useSetRecoilState(sideBarState);
   const { closeModal } = useModalProvider();
 
@@ -41,57 +33,57 @@ export default function StatCard({ nickname, style }: StatCardProps) {
     setSidebar(null);
   };
 
-  if (isLoading) return <LoadingSpinner />;
-  if (isError) return <ErrorRefresher />;
-  const { totalStat, seasonStat, totalRank, seasonRank } =
-    data as StatRankResponse;
-
-  const stats: StatBox[] = [
+  const statTypes = [
     {
-      top: (
-        <div className={styles.top}>
-          <div className={styles.statNameRankWrap}>
-            <div className={styles.statName}>{t('Rank')}</div>
-            <RankTag rank={seasonRank.rank} />
-          </div>
-          <TierLp tier={seasonRank.tier} lp={seasonRank.bestLp} align='start' />
-        </div>
-      ),
-      bottom: <WinRateStat winRateInfo={seasonStat} />,
+      type: 'season',
+      rank: data.seasonRank,
+      stat: data.seasonStat,
+      lpTag: 'ladderTag',
     },
     {
-      top: (
-        <div className={styles.top}>
-          <div className={styles.statName}>{t('Summary')}</div>
-        </div>
-      ),
-      bottom: <WinRateStat winRateInfo={totalStat} />,
+      type: 'total',
+      rank: data.totalRank,
+      stat: data.totalStat,
+      lpTag: 'best',
     },
   ];
 
   return (
     <div className={`${styles.statCardContainer} ${styles[style]}`}>
-      <div className={styles.leftWrap}>
-        {stats.map(({ top, bottom }: StatBox, i: number) => {
-          return (
-            <div key={i} className={styles.leftContent}>
-              {top}
-              {bottom}
-            </div>
-          );
-        })}
-      </div>
-      <div className={styles.rightWrap}>
-        <Link
-          href={`/records/${nickname}`}
-          className={styles.historyLink}
-          onClick={flushOnMove}
-        >
-          <div>{t('History')}</div>
-          <IoIosArrowForward />
-        </Link>
-        <BestRecord totalRank={totalRank} style={style} />
-      </div>
+      <Link
+        href={`/records/${nickname}`}
+        className={styles.historyLink}
+        onClick={flushOnMove}
+      >
+        <div>{t('History')}</div>
+        <IoIosArrowForward />
+      </Link>
+      {isLoading || !data ? (
+        <LoadingSpinner />
+      ) : (
+        <div className={styles.statsWrap}>
+          {statTypes.map(({ type, rank, stat, lpTag }) => {
+            return (
+              <div className={styles.statBox} key={type}>
+                <div className={styles.statNameRankWrap}>
+                  <div className={styles.statName}>{t(type)}</div>
+                  {type === 'season' && <RankTag rank={data.seasonRank.rank} />}
+                </div>
+                <div className={styles.lpRankWrap}>
+                  <div className={styles.lpWrap}>
+                    <span className={styles.lpTag}>{t(lpTag)}</span>
+                    <span className={styles.lp}>
+                      <span className={styles.number}>{rank.bestLp}</span>
+                      <span className={styles.unit}>LP</span>
+                    </span>
+                  </div>
+                </div>
+                <WinRateStat winRateInfo={stat} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
