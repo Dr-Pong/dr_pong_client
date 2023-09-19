45357a0e57926b2738e2f3a15661dfa3971d0444
triplecheeseburger <hangyulkim94@gmail.com>
2023-09-19 20:49:54 +0900

    [feat] 신규 기능들 랭킹 페이지에 넣기 #552

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,19 +1,33 @@
 import useTranslation from 'next-translate/useTranslation';
 
 import React, { ReactElement } from 'react';
+import { BsQuestionSquare } from 'react-icons/bs';
+
+import useModalProvider from 'hooks/useModalProvider';
 
 import PageHeader from 'components/global/PageHeader';
 import AppLayout from 'components/layouts/AppLayout';
 import Footer from 'components/layouts/Footer';
 import LeaderboardFrame from 'components/leaderboard/LeaderboardFrame';
+import SeasonTitle from 'components/leaderboard/SeasonTitle';
 
 import styles from 'styles/leaderboard/Leaderboard.module.scss';
 
 export default function Leaderboard() {
   const { t } = useTranslation('leaderboard');
+  const { useSeasonGuideModal } = useModalProvider();
+
+  const headerButtons = [
+    {
+      value: <BsQuestionSquare />,
+      handleButtonClick: useSeasonGuideModal,
+    },
+  ];
+
   return (
     <div className={styles.leaderboardPageContainer}>
-      <PageHeader title={t('Leaderboard')} />
+      <PageHeader title={t('Leaderboard')} buttons={headerButtons} />
+      <SeasonTitle />
       <LeaderboardFrame />
       <Footer />
     </div>
